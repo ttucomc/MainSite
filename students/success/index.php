a579f19f5f3098a94558b9f7e78d763c8e9a2dba
Mario Jimenez <mariotjimenez@gmail.com>
2015-12-07 16:20:06 -0600

    Student Success Foundation Boxes
    Student success foundation boxes are set, however there is a bug with
    foundation spacing.

--- students/success/index.php
+++ students/success/index.php
@@ -6,7 +6,7 @@
 
 ?>
 
-<DOCTYPE !html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -19,17 +19,17 @@
     <!--<script src="bower_components/modernizr/modernizr.js"></script>-->
 </head>
 <body>
-	
+
 	<!-- Student Success Page title and description -->
 	<seciton>
 
 		<div class="row">
 
-			<div class="medium-8 columns medium-offset-2">
+			<div class="medium-8 columns medium-offset-2 student_success blue">
 
-				<h1 class="student_success">Student Success</h1>
-		
-				<p class="student_success">The College of Media &amp; Communication opened its Center for Success, Outreach  &amp; Engagement in October 2014 as a way to provide students with the best resources to prepare for transitioning from college into their professional careers.</p>
+				<h1>Student Success</h1>
+
+				<p>The College of Media &amp; Communication opened its Center for Success, Outreach  &amp; Engagement in October 2014 as a way to provide students with the best resources to prepare for transitioning from college into their professional careers.</p>
 
 			</div>
 
@@ -37,33 +37,41 @@
 
 	</seciton>
 	<!-- End Student Success Page title and description -->
-	
+
+  <br>
+
 	<!-- Student Success Section -->
 	<section>
-		
-		<div class="row">
-			
-			<!-- Career -->
-			<div>
-				
-			</div>
 
-			<!-- Scholar -->
-			<div>
-				
+		<div class="row">
+
+			<!-- Events -->
+			<div class="medium-3 columns green">
+
+        <br><br><br><br><br><br><br><br>
+
 			</div>
 
 			<!-- Intern -->
-			<div>
-				
+			<div class="medium-3 columns grey">
+          <br><br><br><br><br><br><br><br>
 			</div>
 
-			<!-- Events -->
-			<div>
-				
-			</div>
+			<!-- Scholar -->
+			<div class="medium-3 columns blue">
+
+          <br><br><br><br><br><br><br><br>
 
 			</div>
+
+			<!-- Career -->
+			<div class="medium-3 columns green">
+
+          <br><br><br><br><br><br><br><br>
+
+			</div>
+
+		</div>
 
 	</section>
 	<!-- End Student Success Section -->
@@ -72,7 +80,7 @@
 	<section>
 
 		<!-- Team Profiles -->
-		
+
 
 		<div>
 
@@ -81,8 +89,8 @@
 
 	</section>
 	<!-- End Staff Section -->
-	
-	
+
+
 
 
 	<!-- Footer -->
