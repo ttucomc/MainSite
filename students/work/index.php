27dd4de418aba5a1d8f4be2a32ac05ce984b4b4a
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2016-01-07 11:03:47 -0600

    First commit

---
+++ students/work/index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
+	<title>Student Work | Students | CoMC | TTU</title>
+    <meta name="description" content="Our students here at CoMC are incredible! Here's just some of the cool things our students are producing!">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="../../css/ttu.css">
+    <link rel="stylesheet" type="text/css" href="css/studentwork.css">
+
+	<script src="../../js/modernizr.js"></script>
+	<script src="../../js/ttuglobal.js"></script>
+	<script src="../../js/ttuglobal-onload.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+    <meta itemprop="name" content="Student Work | Students | CoMC | TTU">
+    <meta itemprop="description" content="Our students here at CoMC are incredible! Here's just some of the cool things our students are producing!">
+    <!-- <meta itemprop="image" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg"> -->
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@ttu_comc">
+    <meta name="twitter:title" content="Student Work | Students | CoMC | TTU">
+    <meta name="twitter:description" content="Our students here at CoMC are incredible! Here's just some of the cool things our students are producing!">
+    <meta name="twitter:creator" content="@ttu_comc">
+    <!-- <meta name="twitter:image:src" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg"> -->
+
+
+    <meta property="og:title" content="Student Work | Students | CoMC | TTU"/>
+    <link href="http://www.depts.ttu.edu/comc/students/work/" rel="canonical" />
+    <meta property="og:url" content="http://www.depts.ttu.edu/comc/students/work/"/>
+    <meta property="og:description" content="Our students here at CoMC are incredible! Here's just some of the cool things our students are producing!">
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="CoMC Student Work"/>
+    <!-- <meta property="og:image" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg"/> -->
+
+</head>
+<body>
+	<!-- HEADER AND NAV START -->
+	<div class="page">
+	    <header class="header">
+	        <div class="nav-trigger-wrapper"><a class="nav-trigger" data-click-target=".nav-mobile" href="#">☰</a></div>
+	        <div class="wrapper"><a class="logo" href="http://www.ttu.edu/"><img src="http://cmsdev.ttu.edu/_ttu-template/design/images/logo.png" alt="Texas Tech University"></a>
+				<div class="nav-mobile">
+
+		            <nav class="nav-wrapper">
+		                <ul class="nav-utility nav-inline">
+							<li><a href="http://www.ttu.edu/site/">A-Z Index</a></li>
+							<li><a href="http://directory.texastech.edu/">Directory</a></li>
+							<li><a href="http://www.raiderlink.ttu.edu/">Raiderlink</a></li>
+						</ul>
+	                </nav>
+
+		            <form action="http://google.ttu.edu/search" method="post" class="inline-form search-form">
+						<fieldset>
+							<legend class="search-legend">Search</legend>
+							<label for="q">Search</label>
+							<input type="search" name="q" id="q" class="search-input" placeholder="Search">
+							<input name="site" type="hidden" value="Campus" />
+							<input name="numgm" type="hidden" value="5" />
+							<input name="client" type="hidden" value="texas_tech" />
+							<input name="output" type="hidden" value="xml_no_dtd" />
+							<input name="oe" type="hidden" value="UTF-8" />
+							<input name="proxystylesheet" type="hidden" value="texas_tech" />
+							<div class="search-trigger"><i class="icon-search"></i></div>
+							<label for="searchsubmit">Submit</label>
+							<input type="submit" name="searchsubmit" id="searchsubmit" class="search-submit">
+						</fieldset>
+					</form>
+
+		            <div class="nav-primary-moved"></div>
+	            </div>
+	        </div>
+	        <div class="nav-primary-original">
+	            <nav class="nav-wrapper clearfix">
+	                <ul class="nav-primary nav-inline clearfix">
+						<li><a href="http://www.ttu.edu/admissions/">Admissions, Costs &amp; Aid</a></li>
+						<li><a href="http://www.ttu.edu/majors-and-colleges/">Majors &amp; Colleges</a></li>
+						<li><a href="http://www.vpr.ttu.edu/">Research</a></li>
+						<li><a href="http://www.texastechalumni.org/" target="_blank">Alumni Community</a></li>
+						<li><a href="http://www.ttu.edu/campus-life/">Campus Life</a></li>
+						<li><a href="http://www.texastech.com/" target="_blank">Athletics</a></li>
+						<li><a href="http://www.ttu.edu/about/">About TTU</a></li>
+					</ul>
+	            </nav>
+	        </div>
+	        <nav class="nav-wrapper-lower clearfix">
+	            <div class="nav-trigger-wrapper nav-trigger-wrapper-lower"><a class="nav-trigger icon-down" data-click-target=".nav-primary-lower" href="#"></a></div>
+	            <ul class="nav-primary-lower nav-inline clearfix">
+					<li><a href="/comc/about/">About</a></li><!-- Required, no changes -->
+					<li><a href="/comc/programs/">Programs &amp; Majors</a></li><!-- Required, no changes -->
+					<li><a href="/comc/faculty/">Our People</a></li><!-- Required, no changes -->
+					<li><a href="/comc/students/">Students</a></li><!-- Optional, can be removed, or changed with C&M review -->
+					<li><a href="/comc/graduate/">Graduate Programs</a></li><!-- Optional, can be removed, or changed with C&M review -->
+					<li><a href="/comc/alumni/">Alumni</a></li><!-- Optional, can be removed, or changed with C&M review -->
+					<li><a href="/comc/research/">Research</a></li><!-- Optional, can be removed, or changed with C&M review -->
+					<li><a href="/comc/contact/">Contact Us</a></li><!-- Required, no changes -->
+				</ul>
+	        </nav>
+	    </header>
+	</div>
+	<!-- HEADER AND NAV END -->
+	<!-- TTU BREADCRUMBS AND HEADER 1 START -->
+	<div class="wrapper wrapper-padded-mobile">
+        <div class="wrapper">
+            <div class="wrapper">
+                <ul class="breadcrumbs" style="display: none;">
+                    <li class="breadcrumbs-item"><a href="http://www.ttu.edu/"></a></li>
+                    <li class="breadcrumbs-item"><a href="/comc">College of Media and Communications</a></li>
+                </ul>
+            </div>
+            <div class="content-block">
+               	<div class="columns-wrapper column-flex column-flexwrap audience-nav-placeholder-small">
+                    <div class="column-full column-centered column-padded-horizontal">
+                 	    <h1>Student Work</h1>
+                  	</div>
+    <!-- TTU BREADCRUMBS AND HEADER 1 END -->
+
+
+
+
+
+
+
+        <!-- MAIN EDITS START -->
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+        <!-- MAIN EDITS END -->
+
+
+
+
+
+
+
+
+        <!-- TTU WIDE RED AND FOOTER START -->
+        <div class="wrapper-wide-red">
+            <div class="wrapper">
+                <div class="columns-wrapper column-flex column-flexwrap">
+                    <div class='column-one-third column-centered column-padded-vertical-desktop'>
+                        <div class='fact fact-icon'>
+                            <a href='http://www.visit.ttu.edu/'><h3 class='fact-header fact-header-medium'><i class='icon-sign'></i></h3>
+                                <p class='fact-excerpt'>Visit</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class='column-one-third column-centered column-padded-vertical-desktop'>
+                        <div class='fact fact-icon'>
+                            <a href='http://www.ttu.edu/admissions/'><h3 class='fact-header fact-header-medium'><i class='icon-pencil'></i></h3>
+                                <p class='fact-excerpt'>Apply</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class='column-one-third column-centered column-padded-vertical-desktop'>
+                        <div class='fact fact-icon'>
+                            <a href='/comc/contact/index.php'><h3 class='fact-header fact-header-medium'><i class='icon-phone'></i></h3>
+                                <p class='fact-excerpt'>Contact Us</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <footer class="footer">
+            <ul class="sociallinks footer-sociallinks">
+                <li>
+                    <a href="http://www.facebook.com/TexasTechYou" name="Facebook"	onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-facebook"></i>Facebook</a>
+                </li>
+                <li>
+                    <a href="http://www.twitter.com/texastech/"	name="twitter"		onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-twitter"></i>Twitter</a>
+                </li>
+                <li>
+                    <a href="http://foursquare.com/texastech" name="Foursquare"	onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-foursquare"></i>Foursquare</a>
+                </li>
+                <li>
+                    <a href="http://www.youtube.com/user/texastech/" name="YouTube"		onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-youtube"></i>YouTube</a>
+                </li>
+                <li>
+                    <a href="http://www.flickr.com/photos/texas-tech/" name="Flickr"		onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-flickr"></i>Flickr</a>
+                </li>
+                <li>
+                    <a href="http://followgram.me/texastech"	name="Instagram"	onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-instagram"></i>Instagram</a>
+                </li>
+                <li>
+                    <a href="http://www.linkedin.com/companies/texas-tech-university" name="LinkedIn"	onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-linkedin"></i>LinkedIn</a>
+                </li>
+                <li>
+                    <a href="http://www.ttu.edu/mobile/" 	name="TTUMobile"	onClick="track(buildLink('socialbar',this.name),'outbound',this.href);"><i class="icon-ttu"></i>TTU</a>
+                </li>
+            </ul>
+            <div class="wrapper">
+                <div class="wrapper-footer column-flex">
+                    <div class="column-one-half">
+                        <div class="column-one-half column-centered column-padded-horizontal column-padded-vertical column-border-bottom"><a class="logo logo-small" href="http://www.ttu.edu/"><img src="http://cmsdev.ttu.edu/_ttu-template/design/images/logo-small.png" alt="Texas Tech University"></a></div>
+                        <div class="column-one-half column-padded-horizontal column-padded-vertical column-border-left column-border-bottom"><address class="address address-footer"><span class="street">2500 Broadway</span> <span class="citystate">Lubbock, Texas 79409</span>&nbsp;<span class="phone">806-742-2011</span></address>
+                        </div>
+                        <div class="column-full column-padded-horizontal">
+                            <p class="slogan slogan-large">From here, it's possible</p>
+                        </div>
+                    </div>
+                    <div class="column-one-half column-padded-vertical column-flex">
+                        <div class="column-one-half column-padded-horizontal column-border-left">
+                            <ul class="footer-links">
+                                <li><a href="http://www.txhighereddata.org/approot/resumes/ir_launch.htm" onclick="track('online-inst-resumes','footer',this.href);" target="_blank" style="font-size:1.2em;">Online Institutional Resumes</a></li>
+                                <li><a href="http://www.state.tx.us/" target="_blank">State of Texas</a></li>
+                                <li><a href="http://www2.tsl.state.tx.us/trail/" target="_blank">Statewide Search</a></li>
+                                <li><a href="http://www.texashomelandsecurity.com/" target="_blank">Texas Homeland Security</a></li>
+                                <li><a href="http://veterans.portal.texas.gov/en/Pages/default.aspx" target="_blank">Texas Veterans Portal</a></li>
+                                <li><a href="http://sao.fraud.state.tx.us/" target="_blank">SAO Fraud Reporting</a></li>
+                                <li><a href="http://www.physicalplant.ttu.edu/EnergyManagement/">Energy Management</a></li>
+                                <li><a href="http://www.ttu.edu/policy/">General Policy Information</a></li>
+                                <li><a href="http://www.ttu.edu/courseinfo/">Public Access to Course Info</a></li>
+                                <li><a href="http://www.ttu.edu/emergency/">Emergency Communication Center</a></li>
+                            </ul>
+                        </div>
+                        <div class="column-one-half column-padded-horizontal column-border-left column-copyright">
+                            <ul class="footer-links">
+                                <li><a href="http://www.texastech.edu/">TTU System</a></li>
+                                <li><a href="http://www.ttuhsc.edu/">TTU Health Sciences Center</a></li>
+                                <li><a href="http://elpaso.ttuhsc.edu/">TTUHSC at El Paso</a></li>
+                                <li><a href="http://www.angelo.edu/">Angelo State University</a></li>
+                                <li><a href="http://www.ttu.edu/about/contact.php">Contact Us</a></li>
+                                <li><a href="http://www.ttu.edu/map/">Campus Map</a></li>
+                                <li><a href="http://careers.texastech.edu/">Jobs at TTU</a></li>
+                                <li><a href="http://www.ttu.edu/site/sitemap.php">Site Map</a></li>
+                            </ul>
+                            <div class="copright">
+                                <p>© 2015 <a href="http://www.ttu.edu/">Texas Tech University</a></p>
+                                <p>Updated:
+                                   <a href="http://a.cms.omniupdate.com/10?skin=ttu&amp;account=main&amp;site=comc&amp;action=de&amp;path=/index.pcf" target="_blank">Jul 20, 2015</a>
+                                   4:41 PM
+                                </p>
+                            </div>
+                            <p class="slogan slogan-small">From here, it's possible</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- TTU WIDE RED AND FOOTER END -->
+    </body>
+    </html>
