4f7e4d87f17f648cde9335e540ca56bc90ca2f14
mario <student@adminisorsimac3.ttu.edu>
2016-03-10 16:16:35 -0600

    Last Commit to Git in order to create branch

---
+++ parent-portal/footer.php
@@ -0,0 +1,14 @@
+<!-- Vendor JS -->
+<script src="js/greensock/TweenMax.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+<script src="js/greensock/jquery.gsap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+
+<!-- TTU JS -->
+<script src="js/triangles.js"></script>
+<script src="js/menu.js"></script>
+<script src="js/main.js"></script>
+
+
+</body>
+</html>
--- parent-portal/inc/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	define("BASE_URL","/CoMCSite/parent-portal/");
-	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/CoMCSite/parent-portal/");
