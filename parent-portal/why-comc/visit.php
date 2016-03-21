1c8b0f18edfe853fb8fbaec2e918c676c5058866
mario <student@adminisorsimac3.ttu.edu>
2016-03-21 16:50:12 -0500

    Change images to background, changed colors, added Virtual tour

--- parent-portal/why-comc/visit.php
+++ parent-portal/why-comc/visit.php
@@ -8,29 +8,29 @@
 
 */
 ?>
+<script src="http://cdn.youvisit.com/Assets/js/tour/embed/embed.min.js?v=2.16.03.17"></script>
 
 <section id="visit">
 
   <div class="masterImageContainer">
     <div class="imageContainer">
-      <img src="../img/seal-high.jpg" alt="TTU Seal" />
-      <img src="../img/mem-circle-high.jpg" alt="TTU Memorial Circle" />
-      <img src="../img/cmc-high.jpg" alt="TTU COMC Building" />
-      <img src="../img/library-high.jpg" alt="TTU Library" />
+      <div id="ttu_1"></div>
+      <div id="ttu_2"></div>
+      <div id="ttu_3"></div>
+      <div id="ttu_4"></div>
     </div>
-    <img src="../img/map-marker.png" alt="Location Marker Icon" class="marker" />
+
+    <i class="fa fa-map-marker marker"></i>
     <div class="transparent">
-
+      <div class="dailyTours">
+        <h2 class="dailyTours">Daily Tours</h2>
+        <p>
+          Me? i'm dishonest, and a dishonest man you can always trust to be dishonest.
+          honestly. it's the honest ones you want to watch out for,
+          because you can never predict when they're going to do something incredibly... stupid.
+        </p>
+      </div>
     </div>
-  </div>
-
-  <div class="dailyTours">
-    <h2 class="dailyTours">Daily Tours <hr /></h2>
-    <p>
-      Me? i'm dishonest, and a dishonest man you can always trust to be dishonest.
-      honestly. it's the honest ones you want to watch out for,
-      because you can never predict when they're going to do something incredibly... stupid.
-    </p>
   </div>
 
   <div class="visitLocal">
@@ -47,19 +47,22 @@
     <h2>Virtual Tour <hr /></h2>
       <p>Visit Texas Tech using our virtual tour</p>
 
-      <div>
-        Virtual tour goes here
+      <div class="tourContainer">
+        <a class="virtualtour_embed" title="Virtual Reality, Virtual Tour" href="http://www.youvisit.com" data-platform="v" data-link-type="image" data-inst="59975" data-image-width="100%" data-image-height="100">Virtual Tour</a>
       </div>
   </div>
 
-  <h2>More Information</h2>
+  <div class="moreInfo">
+    <h2>More Information <hr /></h2>
 
-  <p>
-    Find more information here, link here.
-  </p>
-
+    <p>
+      Find more information here, link here.
+    </p>
+  </div>
 
 </section>
+
+<script type="text/javascript" src="http://www.youvisit.com/tour/Embed/js2" defer=""></script>
 
 <script>
 
