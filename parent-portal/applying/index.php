8524b2872b9be1b9488d8a8e60f48c5e02365335
mario <student@adminissimac106.ttu.edu>
2016-04-21 15:38:37 -0500

    Added header and main banner for Apply

--- parent-portal/applying/index.php
+++ parent-portal/applying/index.php
@@ -23,7 +23,66 @@
       </div>
     </section>
 
-    <section>
+    <section id="howToApply">
+
+      <h2>My student is<hr /></h2>
+
+      <div class="applyBanner">
+        <a href='#' class="button">
+          <div class="freshman">Incoming Freshman</div>
+        </a>
+
+        <a href='#' class="button">
+          <div class="transfer">
+            Transfer Student
+          </div>
+        </a>
+
+      </div>
+
+      <div class="freshmanContainer">
+
+        <h3>Incoming Freshman</h3>
+
+        <div>Submit a freshman application at ApplyTexas and pay an application fee.</div>
+
+        <div>Send a official high school transcript with GPA and class rank to the Office of Undergraduate Admission.</div>
+
+        <div>
+          Undergraduate Admissions
+          <br />
+          2500 Broadway
+          <br />
+          Box 45005
+          <br />
+          Lubbock, TX 79409-5005
+          <br />
+          Phone: 806.742.1480
+        </div>
+
+        <div>
+          College entrance test scores, SAT or ACT, send from the testing agency at the time the test is taken.
+        </div>
+
+        <div>
+          <div>
+            Individuals who are not high school graduates may be eligible for admission to Texas Tech University when the following have been submitted:
+          </div>
+          <div>
+            I. Application for Admission.
+            <br />
+            II. Scores on the SAT or ACT.
+            <br />
+            III. Current Application Fee.
+          </div>
+
+        </div>
+
+      </div>
+      <div class="transferContainer">
+
+
+      </div>
 
     </section>
 
