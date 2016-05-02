5560d709691888ea9705d578a83c30bbc70b5e33
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2016-05-02 11:31:58 -0500

    Combining and minimizing css and js

--- parent-portal/inc/footer.php
+++ parent-portal/inc/footer.php
@@ -44,41 +44,10 @@
 </footer>
 
 <!-- Vendor JS -->
-<script src="<?php echo BASE_URL; ?>js/greensock/TweenMax.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.3/ScrollMagic.js"></script>
-<script src="<?php echo BASE_URL; ?>js/scrollmagic/plugins/animation.gsap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
-<script src="<?php echo BASE_URL; ?>js/slick.min.js"></script>
 
 <!-- MAIN JS -->
-<script src="<?php echo BASE_URL; ?>js/triangles.js"></script>
-<script src="<?php echo BASE_URL; ?>js/menu.js"></script>
-<script src="<?php echo BASE_URL; ?>js/main.js"></script>
-
-<script type="text/javascript">
-  $(document).foundation();
-
-  // Kicking off ScrollMagic
-  var controller = new ScrollMagic.Controller();
-
-  // Calling growing h1
-  new ScrollMagic.Scene({
-        triggerElement: '#title',
-        triggerHook: 'onLeave',
-        offset: -125
-      })
-      .setTween(TweenMax.to("#title h1", 0.5, {scale: 10, opacity: 0 }))
-      .addTo(controller);
-
-
-  // Show/Hide Resources in footer
-  $('#resources-btn').click(function() {
-    $('#resources').slideToggle('slow');
-    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
-    $('#resources-btn a').toggleClass('show');
-  });
-
-</script>
+<script src="<?php echo BASE_URL; ?>js/main-min.js"></script>
 
 
 
