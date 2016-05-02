deb5fc5883f83a94ad51e8aea4879bd4db86f823
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2016-05-02 10:55:52 -0500

    Added resources to footer

--- parent-portal/inc/footer.php
+++ parent-portal/inc/footer.php
@@ -1,7 +1,44 @@
 <footer>
-  <div class="row">
-    <div class="medium-4 columns end">
+  <div class="row" data-equalizer>
+    <div class="medium-4 columns" data-equalizer-watch>
       <a href="http://comc.ttu.edu"><img src="<?php echo BASE_URL; ?>img/CoMC-horizontal-logo.svg" alt="College of Media &amp; Communication" title="College of Media &amp; Communication" /></a>
+    </div>
+    <div class="medium-4 columns" id="resources-btn" data-equalizer-watch>
+      <a>More Resources</a>
+    </div>
+  </div>
+  <div class="row" id="resources">
+    <div class="medium-5 medium-offset-1 columns">
+      <ul>
+        <li>
+          <a href="http://www.texastechparents.org/" target="_blank">TTU Parents Association</a>
+        </li>
+        <li>
+          <a href="http://www.depts.ttu.edu/registrar/private/transfer/" target="_blank">TTU Transfer Equivalency</a>
+        </li>
+        <li>
+          <a href="https://www.depts.ttu.edu/tsi/" target="_blank">Texas Success Initiative</a>
+        </li>
+        <li>
+          <a href="http://www.depts.ttu.edu/admissions/visit-events/" target="_blank">Visit TTU</a>
+        </li>
+      </ul>
+    </div>
+    <div class="medium-5 columns end">
+      <ul>
+        <li>
+          <a href="http://www.depts.ttu.edu/admissions/" target="_blank">TTU Admissions</a>
+        </li>
+        <li>
+          <a href="http://www.depts.ttu.edu/admissions/payforcollege/index.php" target="_blank">Pay for College</a>
+        </li>
+        <li>
+          <a href="http://www.depts.ttu.edu/financialaid/netCostCalcHome.php" target="_blank">Net Cost Calculator</a>
+        </li>
+        <li>
+          <a href="http://www.depts.ttu.edu/comc/advising/" target="_blank">Advising</a>
+        </li>
+      </ul>
     </div>
   </div>
 </footer>
@@ -32,6 +69,14 @@
       })
       .setTween(TweenMax.to("#title h1", 0.5, {scale: 10, opacity: 0 }))
       .addTo(controller);
+
+
+  // Show/Hide Resources in footer
+  $('#resources-btn').click(function() {
+    $('#resources').slideToggle('slow');
+    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+    $('#resources-btn a').toggleClass('show');
+  });
 
 </script>
 
