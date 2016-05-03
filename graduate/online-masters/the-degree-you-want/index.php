0643fe4c39cfc8a414303a3f28adce32906ba3cb
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2016-05-03 11:40:20 -0500

    First commit

---
+++ graduate/online-masters/the-degree-you-want/index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
+	<title>The Degree You Want | Online Masters | CoMC</title>
+  <meta name="description" content="The Degree You Want" />
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css" />
+</head>
+<body>
+  <section id="top" class="container-fluid">
+    <div class="row text-center">
+      <div class="col-sm-8 col-sm-offset-2">
+        <h1>The Strategic Communication Master's Degree</h1>
+        <h2>Now Online</h2>
+        <p>
+          Earn your Masters of Strategic Communication degree from Texas Tech University! Accredited, convenient 100% online. Update your skills to be competitive in today's global and digitally focused environment. Learn more now! The degree you want.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-3">
+        <form>
+          <fieldset>
+            <legend>
+              Get more information!
+            </legend>
+            <div class="form-group">
+              <label for="first_name">Name:</label>
+              <input type="text" id="first_name" name="first_name" placeholder="First Name" class="form-control" required />
+              <br />
+              <input type="text" id="last_name" name="last_name" placeholder="Last Name" class="form-control" required />
+            </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" id="email" name="email" class="form-control" required />
+            </div>
+            <div class="form-group">
+              <label for="phone">Phone Number:</label>
+              <input type="tel" id="phone" name="phone" class="form-control" require />
+            </div>
+
+            <input type="submit" value="Submit" class="btn btn-primary btn-block" />
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
