f666361b38046eebc6c9a9146179488283c934af
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2016-08-22 16:44:13 -0500

    Added Think Tank page

---
+++ thinktank/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Think Tank | CoMC | TTU</title>
+
+  <link rel="stylesheet" type="text/css" href="../css/ttu.css">
+  <link rel="stylesheet" type="text/css" href="../css/styles.css">
+  <link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
+
+  <script src="../js/modernizr.js"></script>
+  <script src="../js/ttuglobal.js"></script>
+  <script src="../js/ttuglobal-onload.js"></script>
+  <script src="../js/jquery-ui.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+    <!-- OUR CUSTOM HEAD START -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/screen.css" />
+
+
+    <!-- <meta itemprop="name" content="College of Media &amp; Communication | TTU">
+    <meta itemprop="description" content="Check out the College of Media &amp; Communication at Texas Tech University. Discover the power of being a great communicator.">
+    <meta itemprop="image" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@ttu_comc">
+    <meta name="twitter:title" content="College of Media &amp; Communication | TTU">
+    <meta name="twitter:description" content="Check out the College of Media &amp; Communication at Texas Tech University. Discover the power of being a great communicator.">
+    <meta name="twitter:creator" content="@ttu_comc">
+    <meta name="twitter:image:src" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg">
+
+
+    <meta property="og:title" content="College of Media &amp; Communication | TTU"/>
+    <link href="http://www.depts.ttu.edu/comc/" rel="canonical" />
+    <meta property="og:url" content="http://www.depts.ttu.edu/comc/"/>
+    <meta property="og:description" content="Check out the College of Media &amp; Communication at Texas Tech University. Discover the power of being a great communicator.">
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="College of Media & Communication"/>
+    <meta property="og:image" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg"/> -->
+    <!-- OUR CUSTOM HEAD END -->
+</head>
+<body>
+
+  <?php include '../includes/ttu-body-head.php'; ?>
+  <h1>Think Tank</h1>
+
+
+  <!-- CoMC Edits Start -->
+  <div class="small-12 columns">
+    <section id="about">
+      <div class="row">
+        <div class="small-12 columns">
+          <h2>About</h2>
+          <p>The Think Tank is a collaborative lab space under the Department of Advertising dedicated to inspiring deeper creative thinking in the students of the College of Media &amp; Communication at Texas Tech University.</p>
+        </div>
+      </div>
+      <div class="row" id="points">
+        <div class="medium-4 columns">
+          <?php echo '<h3><i class="fa fa-users fa-3x"></i>Collaborate</h3>'; ?>
+          <p>The Think Tank is a space built for collaboration – for students to work on group projects and for collisions between students in different classes and majors to share ideas on individual projects. </p>
+        </div>
+        <div class="medium-4 columns">
+          <?php echo '<h3><i class="fa fa-cogs fa-3x"></i>Create</h3>'; ?>
+          <p>This is a space where students can feel free to compare and critique ideas and challenge themselves to create something completely new.</p>
+        </div>
+        <div class="medium-4 columns">
+          <?php echo '<h3><i class="fa fa-lightbulb-o fa-3x"></i>Ideas</h3>'; ?>
+          <p>The purpose of the Think Tank is to enhance the level of creative thinking in our students to help guide them to bigger, better, and deeper ideas.</p>
+        </div>
+      </div>
+    </section>
+    <section id="schedule" class="row">
+      <div class="small-12 columns">
+        <h2>Schedule Time</h2>
+        <p>Click here to reserve time in the Think Tank. You can work on individual projects and get help from the director, or work in groups on class projects or whatever you might be working on.</p>
+        <p><small><em>NOTE: This is not a computer lab. You may bring your own laptop into the lab. but this is not a space to sit alone on a computer to study or do homework.</em></small></p>
+        <iframe src="https://app.acuityscheduling.com/schedule.php?owner=12551272" width="100%" height="800" frameBorder="0"></iframe>
+      </div>
+    </section>
+    <section id="events">
+      <div class="row">
+        <div class="small-12 columns">
+          <h2>Upcoming Events</h2>
+          <p>See the calendar below for a list of programs, workshops, and speakers in the Think Tank.</p>
+          <br />
+          <iframe style="border-width: 0;" src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=e53d82h8nq6uegvh03m81o7hj8%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FChicago" height="658" width="1170"><!-- intentionally non-empty iframe; for xsl --></iframe>
+        </div>
+      </div>
+    </section>
+  </div>
+  <script src="https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js" type="text/javascript"></script>
+  <!-- CoMC Edits End -->
+
+
+
+  <?php include '../includes/ttu-body-foot.php'; ?>
+
+</body>
+</html>
