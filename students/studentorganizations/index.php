086eddcd1082fcb48b82eefc90af618374165da6
Justin Lugo <jlug331221@gmail.com>
2016-09-20 13:58:42 -0500

    Create a studentorganization folder to add student organizations page to github.

---
+++ students/studentorganizations/index.php
@@ -0,0 +1,156 @@
+<!-- ADD MAIN CONTENT BELOW THIS LINE -->
+<p><a class="external" href="/sub/universitygrounds/posting.php" target="_blank"><br />Student Organizations Posting Guidelines</a></p>
+<p><strong>Please click each organization for more information.</strong></p>
+<br />
+<div class="jquery-ui-accordion"><!-- start of jQuery-UI Accordion -->
+  <h3>Ad Team</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto: shannon.bichard@ttu.edu"> Shannon Bichard</a></li>
+      <li><strong>About:</strong><br /> The Advertising Team at Texas Tech is a group of students selected by interview to compete in a national student advertising competition. The annual competition is put on by the American Advertising Federation and features a corporate client. Past clients have included Glidden paints, Coca Cola, JCPenney, Yahoo, and State Farm. The client provides a case study outlining the history of its product and current advertising situation. The case study reflects a real world situation to challenge students' critical thinking abilities and creativity. Students must research the product and its competition, identify potential problems and develop an integrated communications campaign for the client. Each student team then pitches its campaign to a panel of judges in April.</li>
+      <li><strong>Meetings:</strong>
+        <ul>
+          <li>Every Tuesday evening in COMC 151.</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <h3>Association for Women in Communications (AWC)</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto: maedwards@wordpub.com"> MaryAnn Edwards</a></li>
+    </ul>
+  </div>
+  <h3>Heads Up Display</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:john.velez@ttu.edu">John Velez</a></li>
+      <li><strong>About:</strong> The purpose of Heads Up Display (HUD) is to provide students with opportunities to improve their abilities relating to the fields of Media, English, PR, Journalism, Visual Arts, Graphic Design and more through creating content such as reviews for movie, television shows, and video game along with special interest segments that are posted and shared through digital and social media platforms (e.g., YouTube, Facebook, Reddit, etc). HUD not only provides an outlet for these creative projects to be produced and showcased to the public; it also archives these projects to build portfolios for individual members' usage in future endeavors.</li>
+    </ul>
+  </div>
+  <h3>Kappa Tau Alpha</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:kevin.stoker@ttu.edu"> Kevin Stoker</a></li>
+    </ul>
+  </div>
+  <h3>Media Entrepreneurship and Innovation Group</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:&nbsp;</strong><a class="mail" href="mailto:geoffrey.graybeal@ttu.edu">Geoffrey Graybeal</a></li>
+      <li><strong>About:</strong> The Media Entrepreneurship and Innovation Group seeks to foster and promote an appreciation of and climate for media entrepreneurship and innovation among students at Texas Tech. The group competes in local, regional and national media entrepreneurship competitions, such as Student Startup Madness, and the Scripps International Innovator's Cup Challenge. The group meets twice a month, including hosting guest speakers with experience starting a media company or entrepreneurial project(s) within an existing media organization. For more information contact <a class="mail" href="mailto:mediaentrepreneurs.comc@ttu.edu">mediaentrepreneurs.comc@ttu.edu</a>.</li>
+    </ul>
+  </div>
+  <h3>Outpost Social Media Lab</h3>
+  <div>
+    <p style="float: right; margin-top: 5px;"><a class="icon-twitter" title="Official Outpost Social Media Lab Twitter" href="https://twitter.com/TTU_Outpost" target="_TOP"><span>Twitter</span></a>&nbsp;<a class="icon-facebook" title="Official Outpost Social Media Lab Facebook Page" href="https://www.facebook.com/OutpostLab" target="_TOP"><span>Facebook</span></a></p>
+    <ul>
+      <li>Website: <a href="/comc/research/outpost/" target="_blank"> http://www.depts.ttu.edu/comc/research/outpost</a></li>
+      <li><strong>Adviser:&nbsp;</strong><a class="mail" href="mailto:lisa.low@ttu.edu">Lisa Low</a></li>
+      <li><strong>Contact the Outpost</strong> <a class="mail" href="mailto:outpost.comc@ttu.edu"> outpost.comc@ttu.edu</a></li>
+      <li><strong>Office:</strong> 251B</li>
+      <li><strong>About:</strong> <br /> The Outpost Social Media Lab @ TTU is the College of Media &amp; Communication’s center for the study of new media. The Outpost provides public relations majors and other students within the College of Media &amp; Communication with real-world experience in the growing field of social media listening, engagement, and analysis. In the facility, undergraduate and graduate students can conduct live monitoring of news and events to provide real-time analysis of public sentiment and reaction across the social web, as well as engage with audiences and curate content on behalf of a variety of organizations. The Outpost features a world-class social media command center powered by HootSuite Enterprise ™. The center is integrated into PR 3315: Online &amp; Digital Public Relations; public relations majors in the class get hands on experience developing and implementing social media campaigns and projects for a variety of clients as well as creating content for the lab’s social media feeds and What’s Trending, a weekly digital media webcast.</li>
+    </ul>
+  </div>
+  <h3>Parliamentary Debate Team</h3>
+  <div>
+    <ul>
+      <li>Website: <a href="/comc/programs/commstudies/debate/" target="_blank"> http://www.depts.ttu.edu/comc/programs/commstudies/debate/</a></li>
+      <li><strong>Director:</strong> <a class="mail" href="/comc/students/adam.testerman@ttu.edu">Adam Testerman</a></li>
+      <li><strong>Office:</strong> 258</li>
+      <li><strong>About:</strong> <br /> Texas Tech University is home to one of the most successful debate programs in the country. TTU Forensics is one of the oldest programs on campus, and has established a legacy of excellence that mirrors the lofty goals and expectations the University has set for itself. The debate program has produced three national championships in the past decade: Two NPDA National Championships (2008 and 2010) and an NPTE top-speaker (2010). The program has placed numerous teams in top 5 finishes at nationals and is generally regarded as a year-in, year-out powerhouse.</li>
+    </ul>
+  </div>
+  <h3>RaiderComm</h3>
+  <div>
+    <p style="float: right; margin-top: 5px;"><a class="icon-twitter" title="Official RaiderComm Twitter" href="https://twitter.com/RaiderCommPR" target="_TOP"><span>Twitter</span></a>&nbsp;<a class="icon-facebook" title="Official RaiderComm Facebook Page" href="https://www.facebook.com/RaidercommPR" target="_TOP"><span>Facebook</span></a></p>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:jody.roginson@ttu.edu"> Jody Roginson</a></li>
+      <li><strong>Student Director:</strong> <a class="mail" href="mailto:dylan.echols@ttu.edu"> Dylan Echols</a></li>
+      <li><strong>Student Org. Office:</strong> COMC 251 A</li>
+      <li><strong>About:</strong> <br /> RaiderComm is a student-run, full-service public relations firm based in the Department of Public Relations at the College of Media &amp; Communication. The firm provides public relations majors with an opportunity to develop as professionals, learn new skills, and apply their coursework to real-world accounts for a wide range of clients. Participation in the firm provides students with portfolio-building opportunities and experience in new business development, client relations, and campaign planning and implementation. Students also have an opportunity to develop their leadership skills by serving as firm directors, account managers, and peer mentors to new account associates.</li>
+      <li><strong>Meetings:</strong><br /> Thursdays from 5-6 p.m.</li>
+    </ul>
+  </div>
+
+  <h3>Raider Report</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:robert.peaslee@ttu.edu"> Robert Peaslee</a></li>
+      <li><strong>About:</strong><br />
+        Raider Report is an entertainment themed sports-talk-show that provides an energetic and engaging atmosphere with its staff and viewers.
+        Run by students, the show is great for people who want to break into the sports or entertainment industry with experience in directing,
+        camera operations, script writing, marketing, on-air-talent, and more. We aspire to bring the entertainment atmosphere of a large network
+        to Texas Tech University through engaging interviews and conversation on air. Raider Report is open to all majors and classifications.
+      </li>
+    </ul>
+  </div>
+
+  <h3>Society of Professional Journalists</h3>
+  <div>
+    <p style="float: right; margin-top: 5px;"><a class="icon-facebook" title="Official Society of Professional Journalists Facebook Page" href="https://www.facebook.com/groups/ttuspj/" target="_TOP"><span>Facebook</span></a></p>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:cam.stone@ttu.edu"> Cam Stone</a></li>
+    </ul>
+  </div>
+  <h3>Tech Advertising Federation</h3>
+  <div>
+    <p style="float: right; margin-top: 5px;"><a class="icon-twitter" title="Official Tech Advertising Federation Twitter" href="https://twitter.com/TechAdFed" target="_TOP"><span>Twitter</span></a>&nbsp;<a class="icon-facebook" title="Official Tech Advertising Federation Facebook Page" href="http://www.facebook.com/groups/133252403437213/" target="_TOP"><span>Facebook</span></a>&nbsp;<a class="icon-linkedin" title="Official Tech Advertising Federation LinkedIn Page" href="http://www.linkedin.com/groups/TAF-Tech-Advertising-Federation-4331383?home=&amp;gid=4331383&amp;trk=anet_ug_hm" target="_TOP"><span>LinkedIn</span></a></p>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:james.hodgins@ttu.edu"> James Hodgins</a></li>
+      <li><strong>About:</strong><br />The Tech Advertising Federation isa chapter of the American Advertising Federation. The organization is designed to provide advertising majors the opportunities they need to be successful in the industry and enhance their learning outside of the classroom. TAF brings in guest speakers from big and small agencies across the U.S. TAF also takes its members on tours of agencies as well as hosts a student advertising competition and other fundraising/philanthropy events. Networking opportunities, critique of portfolio work, and the sharing of internships/jobs/industry knowledge are available.</li>
+      <li><strong>Previous Meeting Recaps:</strong>
+        <ul>
+          <li><strong>September</strong><br /> Tech Advertising Federation’s first meeting of the semester was an informational meeting open to all students. The students enjoyed free Wing Stop food while learning about the perks of a TAF membership. Some of these perks include becoming an American Advertising Federation member, monthly guest speakers, and networking opportunities through semester ad tours. Recent graduate, Taylor Shafner from RD Thomas advertising spoke with the students about his experiences and gave insightful advice on how to get an internship and land a job after college during the Q&amp;A session. Students also won fun door prizes, like Mad Men and Breaking Bad posters.</li>
+          <li><strong>October</strong><br /> Tech Advertising Federation’s second meeting was the first member meeting. It included dinner, an officer election, and two speakers. Papa John’s pizza was provided while two guest speakers, Abbey Daniel, and Steve Smothers spoke. Steve Smothers, VP and Director of Marketing for City Bus, spoke with the students about his experiences and answered member’s questions. TAF’s second speaker, Abbey Daniel, was on Ad Team last year and won the Ad Team’s NYC ADVenture that was sponsored by Joe Fairless, who was also present at the meeting. TAF then had an election for a new Event Coordinator, as the current Event Coordinator is stepping down to study abroad next semester. Congratulations to Ryan Russell for being elected!</li>
+          <li><strong>Lubbock Agency Tour</strong><br /> TAF’s Ad Agency Tour was a two day tour on November 7th and 8th. Members toured KCBD, RD Thomas, Griffin Wink Advertising, Digital Base Productions, Ramar Communications, and The Owen Group. Members were able to meet and network with different employees at each location, learn more about internship opportunities, and enjoyed informative Q&amp;A sessions at each location. It was a great learning experience and networking opportunity for TAF members!</li>
+          <li><strong>TAF OSU Tailgate</strong><br /> TAF held a tailgate for the Tech vs. OSU game. Members, TAF alumni, and faculty came out to enjoy tailgating, getting to know each other, and networking. Thanks to everyone who came out!</li>
+          <li><strong>November</strong><br /> TAF’s last meeting of the semester included two insightful guest speakers and some exciting announcements about the spring semester. If you didn’t make it, you missed out on a home cooked fajita dinner! TAF’s two speakers were Kristy Melcher, the president of Lubbock Advertising Federation and owner of No. 818, and James Hodgens, the program chair for the Lubbock Addy Awards and the director of the School of Music’s Communication division. They spoke with TAF members about the American Advertising Awards, the benefits of entering, and how students can enter. They also answered member questions about preparing themselves after college with advice you can’t learn in the classroom! Olan Farnall also spoke with TAF about the Lubbock Advertising Federation’s Ad Book and how a perk of being apart of TAF is having the opportunity to be apart of the LAF Ad Book. Next, TAF’s sophomore and junior officer selections were announced. Congratulations to Meagan Zenkner and Valerie Partridge! TAF also announced its plans for a spring ad tour and networking events in Dallas, an upcoming holiday party, and a competitive TAF Ad Book that members have the opportunity to be apart of. Lastly, TAF announced a contest for the cover of the TAF Ad Book, which is a great opportunity for the members to get creative and get their work seen by numerous ad agencies in and outside of Lubbock.</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <h3>Tech Association of Student Electronic Media</h3>
+  <div>
+    <p style="float: right; margin-top: 5px;"><a class="icon-facebook" title="Official Tech PR Facebook Page" href="https://www.facebook.com/groups/165017380186664/" target="_TOP"><span>Facebook</span></a></p>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:jerod.w.foster@ttu.edu"> Jerod Foster</a></li>
+    </ul>
+  </div>
+  <h3>Tech PR</h3>
+  <div>
+    <p style="float: right; margin-top: 5px;"><a class="icon-twitter" title="Official TxTechPR Twitter" href="https://twitter.com/TxTechPR" target="_TOP"><span>Twitter</span></a>&nbsp;<a class="icon-facebook" title="Official Tech PR Facebook Page" href="https://www.facebook.com/pages/Tech-PR/143742902306546" target="_TOP"><span>Facebook</span></a></p>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:jo.grant@ttu.edu"> Jo Grant</a></li>
+      <li><strong>Contact Tech PR: </strong><a class="mail" href="mailto:techpr.mcom@ttu.edu">techpr.mcom@ttu.edu</a></li>
+      <li><strong>Student Org. Office:</strong> COMC 271</li>
+      <li><strong>About:</strong><br /> Tech PR is the student public relations association in the College of Media &amp; Communication. The association provides members with career development, professional networking, and portfolio and resume building opportunities. The association hosts monthly meetings that feature practitioners and alumni guest speakers covering a range of topics including corporate communication, public relations firms, political public relations, nonprofit public relations, social media, sports public relations, career development, how to secure internships and jobs, and much more. The association also hosts a number of professional networking, social, and community service events throughout the year. The highlight of the year is the annual spring agency tour in Dallas. Students participating in the day-long trip attend a half-day conference featuring mock interviews and resume critique sessions with Dallas-area alumni and professionals followed by tours of public relations firms and other organizations with large in-house strategic communication departments. The association also affords students with leadership opportunities through service as elected officers or appointed committee chairs. If you have any questions, please email <a class="mail" href="mailto:techpr.mcom@ttu.edu">techpr.mcom@ttu.edu</a>.</li>
+      <li><strong>Meetings:</strong><br /> Second Wednesday of every month at 6 p.m. Room 075.</li>
+    </ul>
+  </div>
+
+  <h3>The Motion Picture Project</h3>
+  <div>
+    <ul>
+      <li><strong>Adviser:</strong> <a class="mail" href="mailto:robert.peaslee@ttu.edu"> Robert Peaslee</a></li>
+      <li><strong>About:</strong><br />
+        The Motion Picture Project is a student-run film organization that provides students with outside-of-the-classroom experience in editing,
+        film production, story boarding, script writing, technique analysis and much more. Our weekly meetings consist of concept development, guest
+        speakers , film technique research and the occasional movie day. The Motion Picture Project is open to students of all majors and classifications.
+      </li>
+    </ul>
+  </div>
+
+</div>
+<!-- end of jQuery-UI Accordion -->
+<script type="text/javascript">// <![CDATA[
+  $( document ).ready(function() {
+    $( ".jquery-ui-accordion" ).accordion({
+      collapsible: true,
+      header: "h3",
+      heightStyle: "content",
+      active: false
+    });
+  });
+  // ]]></script>
+  <!-- ADD MAIN CONTENT ABOVE THIS LINE -->
