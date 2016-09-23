baf723b42594b29b4d5dc823d92e520ca40cb456
Justin Lugo <jlug331221@gmail.com>
2016-09-23 16:36:32 -0500

    Setup intial layout of master page. Using Material Design Lite for styling.

---
+++ faculty/TTUCOMCFacultyCMS/resources/views/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>COMC TTU Faculty CMS</title>
+
+        <link rel="stylesheet" href="css/material-design-lite.css">
+
+    </head>
+
+    <body>
+
+      <div class="mdl-layout mdl-js-layout">
+
+        <header>
+          
+        </header>
+
+        <main>
+          @yield('content')
+        </main>
+
+        <footer>
+
+        </footer>
+
+      </div>
+
+    </body>
+
+</html>
--- faculty/TTUCOMCFacultyCMS/routes/web.php
+++ faculty/TTUCOMCFacultyCMS/routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
+});
+
+Route::get('/test', function() {
+    return view('test');
 });
