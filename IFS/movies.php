04e419ed026007526f4884ca6364527cde0c50ca
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2016-09-30 08:43:40 -0500

    Updated IFS db code

--- IFS/movies.php
+++ IFS/movies.php
@@ -2,7 +2,7 @@
     <div class="small-12 columns card">
         <div class="row collapse card-header" style="background-image:url('images/<?php echo $movie_image; ?>');">
             <div class="small-10 medium-6 columns">
-                <h3><?php echo $movie['title']; ?></h3>
+                <h3><?php echo (($movie['title'] == 'The Handmaiden') ? "<em>Exclusive Sneak Preview of</em><br />" . $movie['title'] : $movie['title']); ?></h3>
             </div>
         </div>
         <div class="row card-body" data-equalizer>
@@ -11,7 +11,7 @@
             </div>
             <div class="small-11 medium-8 columns" data-equalizer-watch>
                 <h4><time datetime="<?php echo $movie['show_date']; ?>"><?php echo $show_date; ?></time></h4>
-                  <p><strong><em><?php echo $movie['title']; ?></em></strong> (<?php echo $movie['rating'] . ' | ' . $movie['length'] . ' min | ' . $movie['release_year'] . ' | dir. ' . $movie['director'] . ' | ' . $movie['country']; ?>)</p>
+                  <p><strong><em><?php echo (($movie['title'] == 'The Handmaiden') ? "Exclusive Sneak Preview of " . $movie['title'] : $movie['title']); ?></em></strong> (<?php echo $movie['rating'] . ' | ' . $movie['length'] . ' min | ' . $movie['release_year'] . ' | dir. ' . $movie['director'] . ' | ' . $movie['country']; ?>)</p>
                   <p>
                     <?php if (!is_null($movie['show_series'])) {
                         echo $movie['show_series'];
---
+++ graduate/ma/tracks/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Tracks | MA | Graduate | CoMC | TTU</title>
+
+	<link rel="stylesheet" type="text/css" href="/comcsite/css/ttu.css">
+	<link rel="stylesheet" type="text/css" href="/comcsite/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="/comcsite/css/jquery-ui.css">
+
+	<script src="/comcsite/js/modernizr.js"></script>
+	<script src="/comcsite/js/ttuglobal.js"></script>
+	<script src="/comcsite/js/ttuglobal-onload.js"></script>
+	<script src="/comcsite/js/jquery-ui.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+    <!-- OUR CUSTOM HEAD START -->
+    <!-- <meta itemprop="name" content="College of Media &amp; Communication | TTU">
+    <meta itemprop="description" content="Check out the College of Media &amp; Communication at Texas Tech University. Discover the power of being a great communicator.">
+    <meta itemprop="image" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@ttu_comc">
+    <meta name="twitter:title" content="College of Media &amp; Communication | TTU">
+    <meta name="twitter:description" content="Check out the College of Media &amp; Communication at Texas Tech University. Discover the power of being a great communicator.">
+    <meta name="twitter:creator" content="@ttu_comc">
+    <meta name="twitter:image:src" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg">
+
+
+    <meta property="og:title" content="College of Media &amp; Communication | TTU"/>
+    <link href="http://www.depts.ttu.edu/comc/" rel="canonical" />
+    <meta property="og:url" content="http://www.depts.ttu.edu/comc/"/>
+    <meta property="og:description" content="Check out the College of Media &amp; Communication at Texas Tech University. Discover the power of being a great communicator.">
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="College of Media & Communication"/>
+    <meta property="og:image" content="http://www.depts.ttu.edu/comc/images/home/og-image.jpg"/> -->
+    <!-- OUR CUSTOM HEAD END -->
+
+</head>
+<body>
+  <?php include "../../../includes/ttu-body-head.php"; ?>
+  <h1>M.A. Program Tracks</h1>
+
+  <!-- ADD MAIN CONTENT BELOW THIS LINE -->
+  <div class="row">
+    <div class="medium-4 columns">
+      <p>
+        <a href="#sports" class="btnlink">Sports Media Track</a>
+      </p>
+    </div>
+    <div class="medium-4 columns">
+      <p>
+        <a href="#thesis" class="btnlink">Thesis Track</a>
+      </p>
+    </div>
+    <div class="medium-4 columns">
+      <p>
+        <a href="#general" class="btnlink">General Professional</a>
+      </p>
+    </div>
+  </div>
+  <h2>All tracks in the Master of Arts in Mass Communication Program require 30 hours.</h2>
+  <ul>
+    <li>The <a href="#sports">sports media track</a> assists students in obtaining jobs in the fast-growing sports entertainment industry.</li>
+    <li>The <a href="#thesis">thesis track</a> prepares students for in-depth studies in theory and research as well provides the foundation for the pursuit of a Ph.D. degree.</li>
+    <li>The <a href="#general">general professional track</a> allows students to develop additional professional skills across a variety of media fields.</li>
+    <li>All non-thesis students will complete a “final comprehensive evaluation” in the form of a capstone final project (MCOM 6050 - Final Project).</li>
+  </ul>
+  <section>
+    <h3 id="sports">Sports Media Track</h3>
+    <h4>Sports media required classes are:</h4>
+    <ul>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5364">MCOM 5364 Research Methods</a></li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5366">MCOM 5366 Seminar in Mass Communication Theory</a></li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+6330">MCOM 6330 Seminar in Media and Sport</a></li>
+      <li>6 hours of approved College Elective Course in Sports Media (course numbers and prefixes may vary; can be in ADV, PR, JEM, MCOM, or COMS)</li>
+      <li>
+        9 hours of additional College Elective Courses, including additional college sports media courses if desired (can be in ADV, PR, JEM, MCOM, or COMS)*
+      </li>
+      <li>
+        3 hours of MCOM 5370 Internship**
+      </li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+6050">MCOM 6050 Final Project</a></li>
+    </ul>
+    <p><em>*Students may take up to six hours of graduate coursework outside of the college in place of up to six hours of College Elective Courses.</em></p>
+    <p>
+      <em>**Sports Track students must complete their MCOM 5370 Internship by working in an approved sport communication setting. This credit requires a minimum of 160 internship hours as well as weekly reports and a formal final evaluation. Internship can be combined with MCOM 6050 for a total of six hours of credit if internship has minimum of 300 hours and MCOM 6050 program and internship overlap.</em>
+    </p>
+    <h4>Concerning Final Project</h4>
+    <p>All Sports Track graduate students will complete a “final comprehensive evaluation” in the form of a capstone final project that focuses on sports media. The course, which will be taken in the student’s final semester:</p>
+    <ul>
+      <li>will use the current COMC 6050 designation.</li>
+      <li>will be listed under a graduate faculty member.</li>
+      <li>will require students to complete a research project focusing on sport media.</li>
+      <li>will require students to orally present assignment to graduate faculty.</li>
+    </ul>
+    <p><a class="topLink" href="#top">[top]</a></p>
+  </section>
+  <section>
+    <h3 id="thesis">Thesis Track</h3>
+    <h4>Thesis track required classes are:</h4>
+    <ul>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5364">MCOM 5364 Research Methods</a></li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5366">MCOM 5366 Seminar in Mass Communication Theory</a></li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5374" target="_blank">MCOM 5374 Data Analysis</a></li>
+      <li>9 to 15 hours of College Elective Course (can be in ADV, PR, JEM, MCOM, or COMS)</li>
+      <li>6 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+6000" target="_blank">MCOM 6000 Thesis</a></li>
+    </ul>
+    <p><em>*Students may take up to six hours of graduate coursework outside of the college in place of up to six hours of College Elective Courses. Common courses outside of the College are:</em></p>
+    <ul>
+      <li><a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=psy+5329" target="_blank">PSY 5329 Emotion</a></li>
+      <li><a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=psy+5356">PSY 5356 Seminar in Cognition</a></li>
+      <li><a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=pols+5321" target="_blank">POLS 5321 Seminar in Political Behavior</a></li>
+      <li>Others at approval of graduate advisor</li>
+    </ul>
+    <p><a class="topLink" href="#top">[top]</a></p>
+  </section>
+  <section>
+    <h3 id="general">General Professional Track</h3>
+    <h4>General professional track required classes are:</h4>
+    <ul>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5364">MCOM 5364 Research Methods</a></li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+5366">MCOM 5366 Seminar in Mass Communication Theory</a></li>
+      <li>15 to 21 hours of College Elective Course (can be in ADV, PR, JEM, MCOM, or COMS)</li>
+      <li>3 hours of <a href="http://catalog.ttu.edu/search_advanced.php?cur_cat_oid=2&amp;search_database=Search&amp;search_db=Search&amp;cpage=1&amp;ecpage=1&amp;ppage=1&amp;spage=1&amp;tpage=1&amp;location=3&amp;filter%5Bkeyword%5D=mcom+6050">MCOM 6050 Final Project</a></li>
+    </ul>
+    <p><em>*Three hours of MCOM 5370 Internship credit is possible under the College Elective Course category. This credit requires a minimum of 160 internship hours as well as weekly reports and a formal final evaluation. Internship can be combined with MCOM 6050 for a total of 6 hours of credit if internship has minimum of 300 hours and MCOM 6050 program and internship overlap.<br /><br /> **Students may take up to six hours of graduate coursework outside of the college in place of up to six hours of College Elective Courses with approval of graduate advisor.</em></p>
+    <p><a class="topLink" href="#top">[top]</a></p>
+  </section>
+  <!-- ADD MAIN CONTENT ABOVE THIS LINE -->
+
+  <?php include "../../../includes/ttu-body-foot.php"; ?>
+</body>
+</html>
