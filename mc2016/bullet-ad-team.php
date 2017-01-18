6a4b3eef7ded86fe246890a0b7e2e189d2388a67
Justin Lugo <jlug331221@gmail.com>
2017-01-18 12:33:41 -0600

    Bullet ad page now complete. Need to go back to the transfer student page and add in the background triangles.

--- mc2016/bullet-ad-team.php
+++ mc2016/bullet-ad-team.php
@@ -20,7 +20,7 @@
             <div class="row">
                 <div class="large-12 columns">
                     <h1>Bullet Ad Team Wins Fourth Place at District Competition</h1>
-                    <p><em><small>By: Rachel Blevins, photos by Eliud Ramirez</small></em></p>
+                    <p><small><em>By: Rachel Blevins, photos by Eliud Ramirez</em></small></p>
                 </div>
             </div>
         </section>
@@ -28,19 +28,38 @@
         <section class="bullet-ad-team-content">
             <div class="row">
                 <div class="large-3 columns">
+                    <img class="bullet-ad-team-logo" src="images/BulletAdTeam.png" alt="Bullet Ad Team Logo">
 
+                    <p class="avenir-next bullet-ad-intro-green-text">The Texas Tech Bullet Advertising Team placed fourth in District 10 in the 2016 American Advertising Federation’s National Student Advertising Competition held April 14-16 in Lubbock.</p>
+
+                    <p>Shannon Bichard, chair of the Department of Advertising and faculty adviser for the ad team, said they started preparing for the competition in Fall 2015. After Bichard handpicked the members based on applications and interviews, they spent months creating a plans book and preparing a 20-minute presentation.</p>
+                    <p>“Their whole campaign was about super short stories with Snapple,” Bichard said. “All of the ads featured people drinking Snapple, and they were all hilarious ‘you had to be there’ moments. <span class="avenir-next-red-text">Everybody was super engaged; that was one thing I noticed.”</span></p>
                 </div>
 
                 <div class="large-3 columns">
+                    <img class="bullet-ad-images" src="images/bullet-ad-1.jpg" alt="Bullet AD Group Discussion">
 
+                    <p>Bichard said that in the last 10 years, the ad team has advanced to nationals twice and usually places in the top three in their district. She noted that during that time, she has realized that the judging process is largely subjective.</p>
+                    <p>“Each year you just don’t know what the judges are looking for,” Bichard said. “My goal is to place in the top five teams. I’ve realized that the top three to five teams are all equally fantastic; it just depends on what resonates with the judges.”</p>
+                    <p>Theodore Vining (BA advertising, 2016) served as a graphic designer for the creative team. He said he applied for the team after he gained experience working as an officer for Tech Advertising Federation.</p>
+                    <p>“For the majority of the year I served on the creative team, helping brainstorm and ideate the campaign’s creative strategy,” Vining said. “After the research and creative strategy were nailed down, I served as chair of the book layout team. During this time, I had the opportunity to lead a team of three other students in designing the 27-page plans book for the campaign."</p>
                 </div>
 
                 <div class="large-3 columns">
+                    <img class="bullet-ad-images" src="images/bullet-ad-2.jpg" alt="Bullet AD team receives questions">
 
+                    <p>Vining said that although the team was a little discouraged by their fourth place finish, he feels their work was not wasted, and he is thankful for the experience.</p>
+                    <p>“There have been many takeaways that come from ad team,” Vining said. <span class="avenir-next avenir-next-green-text">“The work experience is outstanding and is unmatched by any internship I have had.</span> It is probably the closest you can get to experiencing how an advertising agency operates and builds campaigns without actually working at one.”</p>
+                    <p>Victoria Young (BA advertising, 2016) served as the team’s creative director. She said she first heard about the team as a freshman, but it wasn’t until her senior year that she became a member.</p>
+                    <p>“As creative director, I ran a team of five copywriters and designers,” Young said. “We were in charge of creating the big idea for the campaign, so essentially the overarching concept and all the creative executions to go along with it. I was responsible for keeping us organized and on track with our assignments, while also providing creative brainstorms.”</p>
                 </div>
 
                 <div class="large-3 columns">
+                    <img class="bullet-ad-images" src="images/bullet-ad-3.jpg" alt="Bullet AD team presents super short stories">
 
+                    <p>Young said the district competition taught her a very important real-world lesson: it is impossible to please everyone.</p>
+                    <p>“The competition is subjective but so is the real-world client sometimes,” Young said. “It could be a great campaign and still not be a home run. <span class="avenir-next-red-text">I’m so proud of our work</span> but it really made me understand how cutthroat the advertising industry can be. Teamwork makes the dream work but your dream might not be the same as the client’s, and we have to get used to that being a possibility.”<span><img class="small-mc-logo" src="images/MCLogo.png" alt="MC Logo"></span></p>
+                    <p class="bullet-ad-journalists"><small><em>(Rachel Blevins is a junior journalism major from Mineral Wells, Texas. Eliud Ramirez is a junior electronic media and communications major from Donna, Texas.)</em></small></p>
                 </div>
             </div>
         </section>
