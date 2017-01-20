f18ab858497e6f08dd0ac97895d2e0b12d2a36d2
Justin Lugo <jlug331221@gmail.com>
2017-01-20 10:23:37 -0600

    Almost done with the debate team page.

--- mc2016/comc-debate-team.php
+++ mc2016/comc-debate-team.php
@@ -19,47 +19,60 @@
         <section class="comc-debate-team-header-with-content">
             <div class="row">
                 <div class="large-12 columns">
-                    <img class="debate-team-group-image-1" src="images/debate-team-1.jpg" alt="Group Debate Team Image">
+                    <div class="reduce-width-on-large-screens">
+                        <img class="debate-team-group-image-1" src="images/debate-team-1.jpg" alt="Group Debate Team Image">
 
-                    <h1 class="adobe-devanagari-bold-italic text-align-h1-center">CoMC Debate Team</h1>
-                    <h1 class="adobe-devanagari-regular">Launches Record-Breaking Season</h1>
-                    <h1 class="adobe-devanagari-regular">With New Director of Forensics</h1>
+                        <h1 class="adobe-devanagari-bold-italic text-align-h1-center">CoMC Debate Team</h1>
+                        <h1 class="adobe-devanagari-regular h1-letter-spacing-on-large-screens">Launches Record-Breaking Season</h1>
+                        <h1 class="adobe-devanagari-regular h1-letter-spacing-on-large-screens">With New Director of Forensics</h1>
 
-                    <p class="reduce-margin-p-top"><em><small>by Rachel Blevins, photos by Jacob Copple</small></em></p>
+                        <p class="reduce-margin-p-top"><em><small>by Rachel Blevins, photos by Jacob Copple</small></em></p>
+                    </div>
                 </div>
             </div>
 
             <div class="full-width-grey-bg">
                 <div class="row">
-                    <div class="large-4 columns">
+                    <div class="large-6 columns">
                         <p>The Texas Tech Debate Team, housed in the College of Media &amp; Communication, started the 2015-16 season by breaking university records with the help of a new director of forensics.</p>
                         <p>Adam Testerman (BA communication studies, 2012) returned to Texas Tech to work as the director of forensics in July 2015 after earning a master’s in communication studies from Portland State University in May.</p>
                         <p>“I have been debating for a long time,” Testerman said. “I started my freshman year of high school, and I’ve been involved in some capacity ever since.”</p>
                         <p>Originally from Springfield, Mo., Testerman was recruited to the debate team at Texas Tech as an undergraduate student. He helped the team win its second national championship and was the first Texas Tech student to be named the top individual debater in the country at the National Parliamentary Tournament of Excellence in 2010.</p>
                         <p>Texas Tech’s debate team competes in parliamentary style, which Testerman said he enjoys because it presents the contestants with a unique challenge and requires them to think on their feet.</p>
-                    </div>
-
-                    <div class="large-4 columns">
                         <p>“It’s a really unique style because the topic changes every single debate round,” Testerman said. “We get a topic and then have 20 minutes to prepare for it, so it’s a very intense way of doing it. It really brings in a lot of the elements that I think make debate great.”</p>
                         <p>Currently, the team has 13 members, and Testerman said that while anyone is welcome to join, being a member requires commitment, and the team’s schedule can be very demanding.</p>
                         <p>“Anyone who wants to figure out if it’s for them is encouraged to come hang out with us,” Testerman said. “But we have fairly high standards for what it takes to remain a member of the team. There are a lot of expectations for tournament travel, for attending practices, and for researching material that needs to be produced every single week.”</p>
                         <p>When it comes to his vision for the team, Testerman said it relies on two main things: hard work and utilizing university resources. He said he encourages students to take the information they learn in class and to find ways to apply it in a debate context.</p>
+                        <p>"We expect to be the team that is ready for any kind of argument, and we expect to be the best researched team in the nation,” Testerman said. “Other teams might beat us in an individual round, but no one gets to outwork us.”</p>
                     </div>
 
-                    <div class="large-4 columns">
-                        <p>"We expect to be the team that is ready for any kind of argument, and we expect to be the best researched team in the nation,” Testerman said. “Other teams might beat us in an individual round, but no one gets to outwork us.”</p>
+                    <div class="large-6 columns">
                         <p>Testerman said the debate team is one of the oldest programs on campus, and since the Department of Communication Studies merged with CoMC, it has flourished with support from the administrative level.</p>
                         <p>The teams attended three tournaments during Fall 2015, and Testerman said he was very pleased with the results. At the first tournament, two teams made it to the top 16, and Texas Tech finished in fourth place, which Testerman noted is the best performance the team has had in at least 10 years.</p>
                         <p>At the second tournament, one team reached the final round, and Texas Tech won the award for top speaker after three of the team’s speakers reached the top 15. The third tournament of the season took the team to Lewis &amp; Clark College, where Testerman was previously a debate coach.</p>
                         <p>“We did really well at the Lewis &amp; Clark College debate tournament,” Testerman said. “We had two speakers in the top 15, and both of our teams cleared to elimination rounds. One of our teams made it to quarterfinals, securing a finish in the top eight. And our other team made it all the way to semi-finals, securing a finish in the top three teams.”
                         </p>
+                        <p>Testerman noted that this season’s accomplishments have placed three teams from Texas Tech in the top 30 in the country.</p>
+                        <p>The team concluded its competitive season in March by competing in two national championship parliamentary debate tournaments. In the National Parliamentary Tournament of Excellence, Testerman said Texas Tech had a “very impressive” 9-3 preliminary record and cleared to the fourth elimination round, earning them an 11th place finish, which was much higher than they were ranked.</p>
+                        <p>Then, at the second tournament, the National Parliamentary Debate Association, Texas Tech competed against more than 160 teams. Testerman said the college’s senior team made it all the way to the quarterfinal round, placing them in the top eight teams in the nation. While most universities compete with at least four teams, Testerman noted that Texas Tech finished 18th overall in the nation with only two teams competing.</p>
+                        <p>“I am so proud of our debaters’ hard work and dedication to excellence,” Testerman said. “We had just about our best performance of the year at the most important time of year. Our seniors, Drew Hoffmaster and Dalton Richardson, really set an example for what Tech debate is aiming to be, excellent.”<span><img class="small-mc-logo" src="images/MCLogo.png" alt="MC Logo"></span></p></p>
+                        <p class="debate-team-journalists"><small><em>(Rachel Blevins is a junior journalism major from Mineral Wells, Texas.) Jacob Copple graduated in 2015 with a Master of Arts degree in Mass Communications and in 2013 with a Bachelor of Arts degree in electronic media and communications.)</em></small></p>
                     </div>
                 </div>
             </div>
         </section>
 
         <section class="comc-debate-team-quote-with-pictures">
+            <div class="quote">
+                <div class="quotation-marks-bg">
+                    <img class="quotation-mark" src="images/debate-team-quotation-mark.png" alt="Quotation Mark">
 
+                    <img class="quotation-mark rotate-180-deg" src="images/debate-team-quotation-mark.png" alt="Quotation Mark 2">
+                </div>
+                <div class="debate-team-coach-quote">
+                    <img src="images/debate-team-4.png" alt="Debate Team Quote">
+                </div>
+            </div>
         </section>
 
         <?php include 'includes/footer.php'; ?>
