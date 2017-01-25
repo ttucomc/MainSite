70b32d009e6e7c44aee923346221b38cd323e3cc
Justin Lugo <jlug331221@gmail.com>
2017-01-25 09:58:03 -0600

    Dog Raisers content complete. Now to add the colored paws to the page.

--- mc2016/guide-dog-raisers.php
+++ mc2016/guide-dog-raisers.php
@@ -17,12 +17,72 @@
         <?php include 'includes/header-nav.php'; ?>
 
         <div class="guide-dog-raisers-container">
-            <section>
+            <section class="guide-dog-raisers-header">
+                <div class="guide-dog-raisers-header-photos">
+                    <div class="row">
+                        <div class="large-6 columns">
+                            <div class="guide-dog-raisers-image-with-text">
+                                <img class="fanta-photos" src="images/Fanta-10-months.jpg" alt="Fanta at 10 months">
 
+                                <p class="guide-dog-raisers-image-with-text-p">Fanta</p>
+                            </div>
+                        </div>
+
+                        <div class="large-6 columns">
+                            <div class="guide-dog-raisers-image-with-text">
+                                <img class="fanta-photos" src="images/Fanta-Kaitlyn-8-weeks.jpg" alt="Fanta at 8 weeks">
+
+                                <p class="guide-dog-raisers-image-with-text-p">Kaitlyn Beckert</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="large-12 columns">
+                            <h1 class="bebas-neue guide-dog-raisers-header-h1">Mass Communications MA Student Brings Lone Star Guide Dog Raisers to Lubbock</h1>
+
+                            <p><em><small>by Rachel Blevins, photos courtesy Kaitlyn Beckert</small></em></p>
+                        </div>
+                    </div>
+                </div>
             </section>
 
-            <section>
-                
+            <section class=guide-dog-raisers-content>
+                <div class="row">
+                    <div class="large-6 columns">
+                        <p><span class="bebas-neue guide-dog-raisers-content-drop-cap">W</span>hen Kaitlyn Beckert moved to Lubbock to pursue a master’s degree in mass communications at Texas Tech in January 2016, she brought a few of her furry friends on a mission with her.</p>
+                        <p>Beckert became involved with Guide Dogs for the Blind when she was attending Texas Christian University in Fort Worth as an undergraduate student. She said her roommate was blind, and a search for a guide dog led her to the discovery of the organization’s puppy-raising program.</p>
+                        <p>“My roommate from TCU is blind, and she wanted to get a guide dog for herself and I was helping her research when I came across the puppy-raising program online and thought, ‘I need to do this!’” Beckert said.</p>
+                        <p>Before she was able to receive a puppy to train, Beckert said she had to go through training herself to ensure that she was well versed on proper dog handling skills.</p>
+                        <p>“Depending on the kennel club, they usually hold training sessions to teach you everything and then you have to work with the dog so you get hands-on dog training with a program puppy,” Beckert said. “Then you have to puppy-sit a dog before you can get your own puppy to make sure you actually want to do it.”</p>
+                        <p>Beckert said that after she completed the training she was certified as a “puppy raiser,” where her mission is to train and socialize guide dog puppies before they enter formal training at Guide Dogs for the Blind. She said the puppies are typically Labrador retrievers, golden retrievers or a mix of the two.</p>
+                        <p>“We get them at eight weeks old, and we have them until they’re about 15 months,” Beckert said. “Our job is to teach the puppies good house behavior, to respond to commands, and to expose them to a variety of environments.”</p>
+                        <p>Beckert said the puppies learn all of their basic commands by the time they are four months old, and they are then ready to be socialized. She said that because they need to learn how to adapt to various environments, she takes her puppy with her to grocery stores, malls, sporting events, and when allowed—classes.</p>
+                        <p>“We teach them how to think,” Beckert said. “We put them in situations where they have to think, and they have to ignore distractions and resist temptations. They’re not allowed to engage with other dogs.”</p>
+                        <p>After graduating from TCU with a bachelor’s in journalism in May 2015, Beckert said she spent a few months doing freelance work in her hometown of Houston before she decided to pursue a graduate degree. She said she was drawn to the program offered by the Texas Tech College of Media &amp; Communication because it would allow her to earn her master’s degree in just one year.</p>
+                        <p>Beckert said one of the things she wanted to do was to create a branch of puppy-raisers in Lubbock, and after working with the state coordinator, the group Lone Star Guide Dog Raisers – Lubbock was formed, and they had their first “Puppy 101” class in January.</p>
+                        <p>The group received their first puppies in March, and Beckert said they meet for weekly meetings. She said that while it is always hard to say “goodbye” to the puppies at the end of the year, she knows that they have a greater purpose in life as they go on to provide service to those in need.</p>
+                    </div>
+
+                    <div class="large-6 columns">
+                        <div class="guide-dog-raisers-image-with-text">
+                            <img class="guide-dog-raisers-content-group-photo" src="images/dog-raisers-group-photo.jpg" alt="Dog raisers group photo">
+
+                            <p class="guide-dog-raisers-image-with-text-p-text-align-right-and-bottom">Training puppies Fanta and Brandy meet children at the Maxey Community Center's summer camp.</p>
+                        </div>
+
+                        <h2 class="bebas-neue guide-dog-raisers-content-h2">“Our job is to teach the puppies good house behavior, to respond to commands, and to expose them to a variety of environments.” <span class="guide-dog-raisers-content-h2-span">-Kaitlyn Beckert</span></h2>
+                        <p>Vivian Helms, a senior biology and university studies major from Hutto, Texas, said she became a puppy raiser with the Lubbock chapter after meeting Beckert at a student organization fair.</p>
+                        <p>“I saw a woman sitting with a dog that was very well behaved, so I went to talk to her,” Helms said. “I found out that she was from Guide Dogs for the Blind and was trying to start a Lubbock chapter. I have two dogs myself, so I was very interested. I’m so glad that she was able to bring such a wonderful organization to Lubbock.”</p>
+                        <p>Beckert said the group has received a lot of support in the form of both funding and supplies from organizations on campus such as Delta Gamma Fraternity, Miller Girls, and the Tech Lions Club.</p>
+                        <p>Clifford Fedler, a professor of civil, environmental and construction engineering, serves as the president of the Lubbock Lions club and a co-coordinator of the Tech Lions Club. He said he was drawn to working with the Lone Star Guide Dog Raisers because the Lions Club’s motto is “sight first,” and it has a philanthropic history of working to provide eye care and vision screening to children in need.</p>
+                        <p>“I am trying to get the two organizations to work together when and where they can to help create the critical mass of people necessary to make a difference,” Fedler said. “Kaitlyn is a very dynamic student who gives a great deal of herself to provide a service to those in need. It takes a great heart to do what Kaitlyn does, and she should be commended for her service work, especially as a student.”</p>
+                        <p>Alexandra Protopopova, an assistant professor of companion animal science and the faculty advisor for the group, commended Beckert on her service and said she has done a great job working to bring the organization to Lubbock.</p>
+                        <p>“Kaitlyn has done a fantastic job as the founder of the Lone Star Guide Dog Raisers student organization,” Protopopova said. “The Texas Tech community greatly benefits from this organization through the raising of awareness about visual impairment and blindness and about the essential role that service dogs play in people’s lives. At the same time, the students receive hands-on experience being a part of a non-profit organization as well as dog handling. I am excited to see this organization grow over the next few years!”<span><img class="small-mc-logo" src="images/MCLogo.png" alt="MC Logo"></span></p>
+                        <p><em><small>(Rachel Blevins is a junior journalism major from Mineral Wells, Texas)</small></em></p>
+                    </div>
+                </div>
+
             </section>
         </div>
 
