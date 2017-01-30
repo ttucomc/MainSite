7fa496ab9ac60a2f5c9224c57924a235cbeb87dd
Justin Lugo <jlug331221@gmail.com>
2017-01-30 13:02:06 -0600

    Entertainment industry page complete. Beginning Morris Lecture page.

---
+++ mc2016/entertainment-industry.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="images/ttu_logo.svg" type="image/x-icon">
+
+        <title>The MC 2016 | Distinguished Alumna Recognized</title>
+
+        <link rel="stylesheet" href="css/foundation.css">
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+
+    <body>
+        <?php include 'includes/header-nav.php'; ?>
+
+        <section class="entertainment-industry-header">
+            <div class="row">
+                <div class="large-3 columns">
+                    <div class="entertainment-industry-header-img-with-text">
+                        <img class="entertainment-industry-header-img" src="images/entertainment-4.jpg" alt="Lauren with Blake Shelton!!!!!">
+
+                        <p class="entertainment-industry-header-img-p-text"><em>Lauren Locke with Blake Shelton</em></p>
+                    </div>
+                </div>
+
+                <div class="large-3 columns">
+                    <div class="entertainment-industry-header-img-with-text">
+                        <img class="entertainment-industry-header-img" src="images/entertainment-1.jpg" alt="Lauren with Matt Lauer!!!!!!!!!">
+
+                        <p class="entertainment-industry-header-img-p-text"><em>Lauren Locke with Matt Lauer</em></p>
+                    </div>
+                </div>
+
+                <div class="large-3 columns">
+                    <div class="entertainment-industry-header-img-with-text">
+                        <img class="entertainment-industry-header-img" src="images/entertainment-5.jpg" alt="Lauren with Kate Hudson!">
+
+                        <p class="entertainment-industry-header-img-p-text"><em>Lauren Locke with Kate Hudson</em></p>
+                    </div>
+                </div>
+
+                <div class="large-3 columns">
+                    <div class="entertainment-industry-header-img-with-text">
+                        <img class="entertainment-industry-header-img" src="images/entertainment-3.jpg" alt="Lauren with Nick Jonas...">
+
+                        <p class="entertainment-industry-header-img-p-text"><em>Lauren Locke with Nick Jonas</em></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="entertainment-industry-header-title">
+                <div class="large-12 columns">
+                    <div class="entertainment-industry-header-h1-small-on-large-screens">
+                        <h1 class="avenir-next entertainment-industry-header-h1">Taking on the Entertainment Industry in <span class="entertainment-industry-bigger-h1">Today's</span> climate</h1>
+                        <p class=entertainment-industry-header-p><small><em>by Rachel Blevins, photos courtesy Lauren Locke</em></small></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="entertainment-industry-content">
+            <div class="row entertainment-industry-content-img-and-text">
+                <div class="large-6 columns">
+                    <div class="entertainment-industry-content-img-with-text">
+                        <img src="images/entertainment-2.png" alt="Lauren in front of NBC Studios">
+
+                        <p class="entertainment-industry-content-img-p-text"><em>Lauren Locke in New York City</em></p>
+                    </div>
+                </div>
+
+                <div class="large-6 columns">
+                    <p>College of Media &amp; Communication graduate Lauren Locke was hired as a production associate for the “TODAY” show in March after gaining experience interning and working with “The Meredith Vieira Show.”</p>
+                    <p>Locke earned a bachelor’s degree in electronic media &amp; communications in 2013. After taking a year off to work as a consultant with her sorority, Alpha Phi, she returned to Texas Tech in Fall 2014 to pursue a master’s degree in mass communications.</p>
+                    <p>In addition to attending school, Locke worked as a marketing graduate assistant for the University Career Center. Through that job, she said, she learned about NBC’s Campus 2 Career internship program. After signing up, she found out about a possible spot at “The Meredith Vieira Show” by networking with other members of Alpha Phi. And she was selected by NBC.</p>
+                    <p>Locke packed up and moved to New York City, where she interned with the show and was hired as an office production assistant after she obtained her master’s in December. She then worked as a studio production assistant for a month before learning that the show would not be renewed for another season.</p>
+                    <p>“While I was in the process of interviewing for a few jobs, I was contacted by the hiring manager for the ‘TODAY’ show who had received my resumé,” Locke said. “It was truly fate! Not only my internship, but my job as a production assistant at Meredith played a major role in preparing me for this new job.”</p>
+                    <p>Locke said her experience on “The Meredith Vieira Show” working with segments, talent, producers and all of the various dynamics that go into creating a live broadcast prepared her for her current work on the “TODAY” show.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="large-6 columns">
+                    <p>“Every day is a bit different in that every show is different, but each shift has something interesting for me to do,” said Locke, explaining that she rotates between four different shifts with various responsibilities. “It’s a really cool experience to be in a control room with people who have been working on the show for so long and are legends in the news world.”</p>
+                    <p>Locke said that while working on the set of the “TODAY” show, she had the opportunity to work with icons she has looked up to during her time in school.</p>
+                    <p>“I love getting to interact with people like Hoda Kotb, Jenna Bush Hager, Savannah Guthrie, and all of the talent,” Locke said. “Everyone is truly so genuine and extremely nice, which makes such a fun experience to work on the top news show in the country. It’s also really fun getting to see the celebrities who come on the show.”</p>
+                    <p>Locke said learning from her role models began when she interned with Vieira. She even had the opportunity to learn that Vieira has a sense of humor when she and the other interns were pranked by Vieira, Regis Philbin and a pig.</p>
+                    <p>Locke said it started out with Vieira telling the show’s interns that she needed them to look after a pig she was fostering. She then told them that the pig’s name was “Regis,” but that Regis Philbin was coming by the studio, and they were not allowed to reveal the pig’s real name.</p>
+                    <p>“I didn’t know what was going on at first,” Locke said. “Meredith is such a nice person, and when she told us she was fostering a pig, we thought nothing of it. But then as the prank went on, Regis Philbin was so persistent, and he kept staring at me.”</p>
+                </div>
+
+                <div class="large-6 columns">
+                    <p>Locke ended up being the one to tell Philbin the pig’s real name, and after a stern Vieira re-entered the room, the interns found out that the joke was on them. Locke said it was all in good fun and that the interns had the opportunity to appear on the show the next day to discuss what happened.</p>
+                    <p>When it comes to advising current Texas Tech students, Locke said she encourages them to pursue internships, independent studies, and any experience that will prepare them for the real world.</p>
+                    <p>In addition to her work with “The Meredith Vieira Show,” as a graduate student Locke completed an independent study with MCTV, the college weekly student newscast. During her undergraduate career, she did internships with KCBD News Channel 11 in Lubbock and the Texas Film Commission in Austin. Locke said that if it weren’t for those experiences, she wouldn’t be where she is today.</p>
+                    <p>“My advice for Texas Tech students is to get as much experience as you can and to meet as many people as you can,” Locke said. “Networking is absolutely huge! That’s how I wound up where I am right now. Do your research on the types of jobs that are in the industry that you’re interested in.”</p>
+                    <p>Locke concluded, “Don’t be scared to put yourself out there and to try to get your foot in the door, even if you don’t know exactly what you want to do. You never know what opportunity will present itself, who you’ll meet, or how they’ll direct you.”<span><img class="small-mc-logo" src="images/MCLogo.png" alt="MC Logo"></span></p>
+                    <p><em><small>(Rachel Blevins is a junior journalism major from Mineral Wells, Texas.)</small></em></p>
+                </div>
+            </div>
+        </section>
+
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
