9b5be64264eb509daa875a3d159fd1b431212670
Justin Lugo <jlug331221@gmail.com>
2017-04-03 14:20:43 -0500

    Marcus Pauda page is now complete. Moving onto the Marine Corps page OORAHgit statusgit statusgit statusgit statusgit status

--- mc2016/comm-studies-grad-sports-marketing-firm.php
+++ mc2016/comm-studies-grad-sports-marketing-firm.php
@@ -17,11 +17,61 @@
         <?php include 'includes/header-nav.php'; ?>
 
         <section class="sports-marketing-firm-header">
-            
+            <div class="row">
+                <div class="large-12 columns">
+                    <img src="images/PaudaDeanoftheGame.jpg" alt="Pauda is the Dean of the Game">
+                    <p class="sports-marketing-firm-header-p-text"><em><small>Marcus Pauda greets a fan on the court.</small></em></p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="large-6 columns">
+                    <p class="sports-marketing-firm-header-p-marcus-pauda-quote francois-one">“I love collegiate athletics, and I’m proud to have a degree from the best school in the nation, Texas Tech.”</p>
+                    <p class="sports-marketing-firm-header-p-marcus-pauda francois-one"><em>- Marcus Pauda</em></p>
+                </div>
+
+                <div class="large-6 columns">
+                    <div class="sports-marketing-firm-header-image-with-text">
+                        <img src="images/PaudaHeadShot.jpg" alt="Marcus Puada">
+
+                        <p class="sports-marketing-firm-header-image-p-text">Marcus Pauda</p>
+                    </div>
+                </div>
+            </div>
         </section>
 
-        <section class="sports-marketing-firm-content">
+        <hr class="sports-marketing-firm-hr">
 
+        <section class="sports-marketing-firm-content">
+            <div class="row">
+                <div class="large-12 columns">
+                    <h1 class="sports-marketing-firm-content-h1 francois-one">Communication Studies Graduate Lands Management Job with Top Sports Marketing Firm</h1>
+
+                    <p><em><small>by Rachel Blevins, photos courtesy Marcus Pauda</small></em></p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="large-6 columns">
+                    <p>Marcus Pauda (BA communication studies, 2012) landed a dream job with Learfield Sports in July 2015, which he described as one of the top collegiate sports marketing firms in the country.</p>
+                    <p>“Learfield Sports is by far one of the most prestigious collegiate marketing firms in the country,” Pauda said. “We represent over 125 collegiate athletic programs, and when a college trusts our company to come in and be its sports marketing arm for its athletics program, we send a team out and into the community, and we act as representatives of that athletics program.”</p>
+                    <p>Pauda said he started working for Learfield after he was one of two people chosen from the Learfield Sports Minority Academy initiative, which surveyed more than 100 minority applicants across the country.</p>
+                    <p>“It was a huge honor and something I am really proud of,” Pauda said. “Once the company hires you, they send you out to work for an athletics program somewhere across the country.”</p>
+                    <p>Although his dream destination is to eventually work with Texas Tech on behalf of Learfield, Pauda said the company started him out with the New Mexico Lobos, where he will gain experience as a manager of business development working with the athletic department at the University of New Mexico.</p>
+                    <p>“I work with my clients to make sure that every single one of them is taken care of in terms of fulfillment,” Pauda said. “When I sign a company up to be a corporate partner of the athletics department, my job is to make sure that they’re getting everything that they’re paying for in their contract.”</p>
+                    <p>After earning his bachelor’s degree in December 2012, Pauda started working on the sales team for Texas Tech Athletics. He said the year and a half he spent with Texas Tech Athletics taught him a lot about all of the less obvious work that goes on in sports marketing.</p>
+                </div>
+
+                <div class="large-6 columns">
+                    <p>“People don’t really understand how much work goes on behind the scenes,” Pauda said. “It takes a strong team to get a stadium filled with people. I worked on the sales team in group sales, and I tried to get groups to come to all major sporting events.”</p>
+                    <p>In July 2014, Pauda said he took a job with the University of Miami as the account executive for corporate ticket sales for one year, where he sold club seating and suites to companies.</p>
+                    <p>Looking back on his journey, Pauda said he would encourage students to gain as much experience as possible during college. He said studying abroad in Spain as an undergraduate student and interning with College Hotspots magazine gave him valuable experience that prepared him for his career.</p>
+                    <p>“I don’t think people realize how important internships are,” Pauda said. “I interned with College Hotspots magazine, on the Texas Tech campus, and that’s where I got a lot of my experience, from sales to marketing. My final ending title with the magazine was associate publisher.”</p>
+                    <p>Pauda said he is happy to have a career where he has the opportunity to use the resources offered by a university, along with the excitement of its fans, to help connect their companies with the university’s brand.</p>
+                    <p>“I love collegiate athletics, and I’m proud to have a degree from the best school in the nation, Texas Tech,” Pauda said. “With that, I continue to work with companies around the community and throughout the entire state to help accomplish their marketing and branding needs.”<span><img class="small-mc-logo" src="images/MCLogo.png" alt="MC Logo"></span></p>
+                    <p><em>(Rachel Blevins is a junior journalism major from Mineral Wells, Texas.)</em></p>
+                </div>
+            </div>
         </section>
 
         <?php include 'includes/footer.php'; ?>
