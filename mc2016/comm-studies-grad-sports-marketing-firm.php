b599a6757591256f738439411deb0b0b9bc0f0d6
Justin Lugo <jlug331221@gmail.com>
2017-04-03 10:30:47 -0500

    Basic layout of Marcus Pauda page complete. Still need to import fonts and images from in design file.

---
+++ mc2016/comm-studies-grad-sports-marketing-firm.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="images/ttu_logo.svg" type="image/x-icon">
+
+        <title>The MC 2016 | CNN Atlanta Intern</title>
+
+        <link rel="stylesheet" href="css/foundation.css">
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+
+    <body>
+        <?php include 'includes/header-nav.php'; ?>
+
+        <section class="sports-marketing-firm-header">
+            
+        </section>
+
+        <section class="sports-marketing-firm-content">
+
+        </section>
+
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
