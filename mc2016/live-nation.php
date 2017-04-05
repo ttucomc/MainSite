fb63dfbbfb8556bfd3b0f06b7e18b4cda3b86590
Justin Lugo <jlug331221@gmail.com>
2017-04-05 14:09:20 -0500

    Live nation page complete. Moving onto Panamericana page.

--- mc2016/live-nation.php
+++ mc2016/live-nation.php
@@ -7,7 +7,7 @@
 
         <link rel="shortcut icon" href="images/ttu_logo.svg" type="image/x-icon">
 
-        <title>The MC 2016 | USMC EMC Major</title>
+        <title>The MC 2016 | Live Nation</title>
 
         <link rel="stylesheet" href="css/foundation.css">
         <link rel="stylesheet" href="css/master.css">
@@ -17,11 +17,109 @@
         <?php include 'includes/header-nav.php'; ?>
 
         <section class="live-nation-header">
+            <div class="row">
+                <div class="large-5 columns">
+                    <div class="live-nation-header-image-with-text">
+                        <img src="images/TeaganRaher.jpg" alt="Teagan Raher">
 
+                        <p class="live-nation-header-image-p-text master-of-break">Teagan Raher</p>
+                    </div>
+                </div>
+
+                <div class="large-7 columns live-nation-header-dark-grey-bg">
+                    <div class="_idGenObjectLayout-1">
+            			<div id="_idContainer006">
+            				<div id="_idContainer000" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer001" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer002" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer003" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer004" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer005" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            			</div>
+            		</div>
+
+                    <h1 class="live-nation-header-h1 bebas">From Backstage to Center Stage:</h1>
+
+                    <h3 class="live-nation-header-h3 master-of-break">Pursuing a Career with Live Nation</h3>
+
+                    <p class="live-nation-header-p"><em><small>by Rachel Blevins, photos by Jacob Copple and courtesy Teagan Raher</small></em></p>
+
+                    <div class="_idGenObjectLayout-1">
+            			<div id="_idContainer006">
+            				<div id="_idContainer000" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer001" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer002" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer003" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer004" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer005" class="live-nation-header_idGenObjectStyleOverride-1">
+            				</div>
+            			</div>
+            		</div>
+                </div>
+            </div>
         </section>
 
         <section class="live-nation-content">
+            <div class="row live-nation-content-grey-circles">
+                <div class="large-12 columns">
+                    <div class="_idGenObjectLayout-1">
+            			<div id="_idContainer006">
+            				<div id="_idContainer000" class="live-nation-content_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer001" class="live-nation-content_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer002" class="live-nation-content_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer003" class="live-nation-content_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer004" class="live-nation-content_idGenObjectStyleOverride-1">
+            				</div>
+            				<div id="_idContainer005" class="live-nation-content_idGenObjectStyleOverride-1">
+            				</div>
+            			</div>
+            		</div>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="large-6 columns">
+                    <p>One College of Media &amp; Communication student wrapped up her graduate degree by including an internship that landed her a job as the marketing coordinator for Live Nation Dallas, a company that promotes concerts and other entertainment.</p>
+                    <p>Teagan Raher (BA electronic media and communications, 2014, and MA in mass communications, 2015), from Frisco, Texas, interned with Live Nation in fall 2015 and said she chose the internship because she saw it as a great way to combine her love for music with her love for communication.</p>
+                    <p>“I knew I loved music, and I loved working in communications,” Raher said. “I’ve been running a music blog for the last two years and worked in a marketing office during college, so I wanted some way to combine the two worlds. When the internship experience at Live Nation came around, I knew it would be a great fit.”</p>
+                    <p>Raher said she was familiar with Live Nation, having purchased many concert tickets from the company, and she would occasionally check out their career website for internship opportunities.</p>
+                    <p>“I have a big passion for music and the entertainment industry,” Raher said. “A few years ago I had a really great customer service experience with Live Nation Dallas, and from then on, the company always had a positive presence in my mind.”</p>
+                    <p>During her internship, Raher said she worked in Live Nation’s marketing department where she had the opportunity to learn about all of the different aspects that go into preparing for a show.</p>
+                    <p>“I assisted the marketing department in creating and executing campaigns for concerts and tours that visited the Dallas market,” Raher said. “I helped with grassroots marketing campaigns, updating websites, press releases, accounting, and coordinating press and media for shows."</p>
+                    <p>One of the unique parts about her job was that in addition to preparing for the shows, Raher also had the opportunity to enjoy them herself.</p>
+                    <p>“The most rewarding part of my experience was getting to attend a concert and looking at how excited the crowd was to see their favorite artist live,” Raher said. “One memory that sticks out to me during my internship was the night I worked my first concert, Van Halen. I helped escort the photographers into the stage pit to take pictures. I’ve never been that close to the stage before at a concert, so looking up and seeing Eddie Van Halen shred his guitar in my face is something I’ll never forget.”</p>
+                    <p>Raher said while she was a student at Texas Tech, she gained experience by working as a community adviser, as an assistant for social media and marketing for University Student Housing, and by interning with Strauss Marketing and PR in Dallas.</p>
+                    <p>Raher also said she was influenced by several professors within CoMC who taught her about media, marketing, different cultures and proper grammar.</p>
+                    <p>“Dr. Chambers taught me that media and rock n’ roll are cool industries. Dr. Foster inspired my creativity. Dr. Stone encouraged me to get out of my comfort zone and to try new things. Professor Roginson taught me to chase my passion, no matter how crazy,” Raher said. “Dr. Peaslee motivated my interest in exploring other cultures and ideas. Dr. Ortiz influenced my interest in marketing and its challenging and rewarding ways. Professor Wernsman taught me to proof, proof, and proof, and that exclamation points may not always be appropriate.”</p>
+                </div>
+
+                <div class="large-6 columns">
+                    <p>Todd Chambers, Ph.D., associate dean for undergraduate affairs in CoMC, said Raher was the perfect picture of a student who pursued a field she was truly passionate about. He credits her work ethic, creativity and intellect for helping her turn an internship into a full-time job.</p>
+                    <p>“Teagan will forever be ‘that student’ who gets used as an example for students interested in the music industry,” Chambers said. “Although I didn’t teach her at the undergraduate level, she was in one of my graduate courses called Contemporary Issues in Communication Technology.  She mentioned that she was very interested in music and told me about her blog.  We spent a lot of time talking about her dreams and career aspirations, and she was able to work with Live Nation for her master’s project.”</p>
+                    <p>Rob Peaslee, Ph.D., chairperson for the Department of Journalism &amp; Electronic Media, said Raher was great to work with as a student, and he is sure she will carry the same qualities into her career. </p>
+                    <p>“Teagan Raher is one of the brightest and most reliable students I’ve taught at Tech,” Peaslee said. “It comes as no surprise to me whatsoever that she’s on to grand adventures, and I’m sure she’ll make the most of them. She’s a pleasure to work with, and I’m sure her colleagues at Live Nation Dallas will be duly impressed.”</p>
+                    <p>Raher said she chose to pursue the one-year plan for a master’s degree in mass communications because it seemed like the best step forward. She said it is something she would recommend to students who are passionate about their field of study and interested in research.</p>
+                    <p>“I really enjoyed my time as an undergrad at Texas Tech and, even more so, working with the awesome professors of the College of Media &amp; Communication,” Raher said. “I was passionate about what I was learning and was eager to learn more and research more, so grad school seemed natural as a next step.”</p>
+                    <p>After her internship at Live Nation ended and she graduated with her master’s degree, Raher said she was chosen for a position working in the company’s marketing department in Dallas.</p>
+                    <p>“I’m now the new marketing coordinator at Live Nation Dallas,” Raher said. “I started working immediately after graduation in December 2015 and haven’t looked back. It’s been wild getting adjusted to the working world, but I pinch myself every day when I think about how much I love my job and this company.”<span><img class="small-mc-logo" src="images/MCLogo.png" alt="MC Logo"></span></p>
+                    <p><em>(Rachel Blevins is a junior journalism major from Mineral Wells, Texas. Jacob Copple graduated in 2015 with a Master of Arts degree in Mass Communications and in 2013 with a Bachelor of Arts degree in Electronic Media and Communications.)</em></p>
+                </div>
+            </div>
         </section>
 
         <?php include 'includes/footer.php'; ?>
