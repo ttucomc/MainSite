3cfef206907b1f77384185739cd6c060094e656f
Justin Lugo <jlug331221@gmail.com>
2017-04-12 09:08:01 -0500

    Director of Dev. page complete.

---
+++ mc2016/director-of-development.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="images/ttu_logo.svg" type="image/x-icon">
+
+        <title>The MC 2016 | Director of Development</title>
+
+        <link rel="stylesheet" href="css/foundation.css">
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+
+    <body>
+        <?php include 'includes/header-nav.php'; ?>
+
+        <section class="director-of-development-content">
+            <div class="row">
+                <div class="large-10 large-offset-1 columns">
+                    <div class="director-of-development-content-img-with-title">
+                        <img class="director-of-development-content-img" src="images/ColleenSisneros.jpg" alt="Colleen Sisneros">
+
+                        <div class="director-of-development-content-div-p-border-left">
+                            <p class="director-of-development-content-p-top-image-title-first-child"><em><strong>Director of Development</strong></em></p>
+
+                            <p>Colleen Sisneros, M.A.</p>
+                        </div>
+                    </div>
+
+                    <p>I am thrilled to join the College of Media &amp; Communication as the new director of development. While I am new to CoMC, I am not new to Texas Tech. I am a proud Texas Tech alumna, and I most recently worked for the College of Arts &amp; Sciences as their director of recruitment and retention.</p>
+                    <p>This is an exciting and dynamic time for the college. CoMC reached unprecedented heights in 2015: enrollment reached a five-year high of 1,460 undergraduates; our one-year retention rate of 88 percent well surpassed the university average of 84 percent; we created the online master’s program; and we welcomed the Department of Communication Studies to the college.</p>
+                    <p>My fundraising goals are to support this rapid growth by increasing our endowments for student scholarships, professorships and lectureships, as well as general program support for the college and its new initiatives. Funding for our students and college remains one of the most important tools in recruiting exceptional students and maintaining our academic excellence.</p>
+                    <p>As CoMC pushes steadily forward, we must build and maintain a solid foundation of resources and support. The continued and increased contributions from our friends, alumni, foundations and corporations can help provide the essential resources vital to advancing the mission of CoMC and Texas Tech as a whole.</p>
+                    <p>I look forward to meeting each of you and learning about your unique passions as we work together to move CoMC toward even greater heights.</p>
+                </div>
+            </div>
+        </section>
+
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
