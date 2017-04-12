8bbfadd3162e57835acc79e312d236345f6d9920
Justin Lugo <jlug331221@gmail.com>
2017-04-12 08:07:38 -0500

    Associate Dean of Grad programs done. On to Associate Dean for Undergrad affairs.

--- mc2016/associate-dean-grad-programs.php
+++ mc2016/associate-dean-grad-programs.php
@@ -17,7 +17,22 @@
         <?php include 'includes/header-nav.php'; ?>
 
         <section class="associate-dean-grad-programs-content">
-            
+            <div class="row">
+                <div class="large-3 columns">
+                    <img src="images/CoyCallison.jpg" alt="Dr. Callison">
+
+                    <div class="associate-dean-grad-programs-content-div-top-image-title">
+                        <p class="associate-dean-grad-programs-content-p-top-image-title-first-child"><em><strong>Associate Dean of Gradute Programs</strong></em></p>
+                        <p>Coy Callison, Ph.D.</p>
+                    </div>
+                </div>
+
+                <div class="large-9 columns">
+                    <p><span class="associate-dean-grad-programs-content-drop-letter">A</span>s I started to write this note, I got the idea of looking back at some of the past updates I have    provided over the last few years. After a quick computer search, the first document that pulled up was my letter from summer of 2013. In that letter, I wrote about the excitement that was surrounding the College of Media &amp; Communication’s graduate program growth to nearly 80 students. Three years later, as I write this update, our enrollment has doubled that milestone, and I am proud to say that 160 graduate students, spread across three M.A. programs and a Ph.D. program, are working on degrees. And I see no sign that our growth trend is coming to an end. Of course, at some point we will reach a carrying capacity that we will not want to exceed based on our resources, but so far the college and the university both have been generous in allocating administrative support, funding and personnel to match our rise in enrollment. One great example of how all those resources can come together and help us reach goals centers on the online M.A. program.</p>
+                    <p>After some careful market analysis and extensive planning a few years ago, we elected to lay the groundwork for offering an online M.A. in Strategic Communication and Innovation. The first, and maybe most important, step we took was tapping Kristi Gilmore, Ph.D., to head that program. Kristi told me in one of our first meetings that she wasn’t interested in slowly wading into this online business, and that she was planning on making waves from day one. Well, she has undoubtedly done just that. And in only nine months of offering the program, she told me recently, we have 61 students in the online program for Fall 2016. She has focused on recruiting and enrolling students with professional backgrounds from not only around the state but also from around the nation. And we find it most humorous that a couple of our students are employees at other “big brand” universities in the state. We like the idea of graduate students bearing our banners in university office buildings southeast of here.</p>
+                    <p>In addition to the work Kristi is doing, Mark Gring, Ph.D., has helped us see an uptick in the number of applications for and students enrolled in the Communication Studies M.A. program. And new Assistant Dean for Graduate Studies Trent Seltzer, Ph.D., along with Administrative Assistant Bridget Christopherson, have played huge roles in recruiting our incoming cohort of Ph.D. students while also leading efforts in streamlining office procedures. All in all, in closing I feel as if I could copy and paste from past updates by stating that we continue to see great growth and quality in our graduate programs as a result of the hard work of graduate office staff and the graduate faculty. I suspect that it won’t be long until we have 200 graduate students in the program and we break into a collective grin at the thought of our being so pleased with growing to 186 in the fall of 2016.</p>
+                </div>
+            </div>
         </section>
 
         <?php include 'includes/footer.php'; ?>
