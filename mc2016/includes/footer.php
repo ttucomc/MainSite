87e50a7fa24c09587172fb1aea2e85069a60bf15
Justin Lugo <jlug331221@gmail.com>
2017-04-24 10:17:54 -0500

    Changes to images so that they can be uploaded to Omni Update.

--- mc2016/includes/footer.php
+++ mc2016/includes/footer.php
@@ -5,14 +5,8 @@
 <script src="js/vendor/jquery.js"></script>
 <script src="js/foundation.min.js"></script>
 <script src="js/foundation/foundation.equalizer.js"></script>
-<script src="js/masonry-layout.js"></script>
 <script>
     $(document).ready(function() {
         $(document).foundation();
-
-
-        var $grid = $('.grid').masonry({
-            // options...
-        });
     });
 </script>
--- mc2016/index.php
+++ mc2016/index.php
@@ -16,46 +16,46 @@
     <body>
         <?php include 'includes/header-nav.php'; ?>
 
-        <section class="main-section">
-            <!-- MAIN CONTENT START -->
-			<div id="mc-header">
-				<div class="row logo-row">
-					<div class="large-6 columns logo">
-						mc
-					</div>
-					<div class="small-4 large-2 columns mc-header-border">
-						<br>
-						<p class="mc-header-p">CoMC Debate Team Excels with New Coach</p>
-					</div>
-					<div class="small-4 large-2 columns mc-header-border">
-						<br>
-						<p class="mc-header-p">CoMC Becomes Home for New Communication Training Center</p>
-					</div>
-					<div class="small-4 large-2 columns">
-						<br>
-						<p class="mc-header-p">EMC Major Attends CoMC After Travels with Marine Corps</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="large-12 columns" id="mc-header-tagline">
-						<p style="text-align: center;"><em>A publication for alumni and friends of the Texas Tech University College of Media &amp; Communication • 2016</em></p>
-					</div>
-				</div>
-                <div class="row">
-                    <div class="large-12 columns" style="text-align: center;">
-                        A REVIEW OF ACADEMIC YEAR 2015-2016
-                    </div>
-                </div>
-			</div>
-        </section>
+                <section class="main-section">
+                    <!-- MAIN CONTENT START -->
+        			<div id="mc-header">
+        				<div class="row logo-row">
+        					<div class="large-6 columns logo">
+        						mc
+        					</div>
+        					<div class="small-4 large-2 columns mc-header-border">
+        						<br>
+        						<p class="mc-header-p">CoMC Debate Team Excels with New Coach</p>
+        					</div>
+        					<div class="small-4 large-2 columns mc-header-border">
+        						<br>
+        						<p class="mc-header-p">CoMC Becomes Home for New Communication Training Center</p>
+        					</div>
+        					<div class="small-4 large-2 columns">
+        						<br>
+        						<p class="mc-header-p">EMC Major Attends CoMC After Travels with Marine Corps</p>
+        					</div>
+        				</div>
+        				<div class="row">
+        					<div class="large-12 columns" id="mc-header-tagline">
+        						<p style="text-align: center;"><em>A publication for alumni and friends of the Texas Tech University College of Media &amp; Communication • 2016</em></p>
+        					</div>
+        				</div>
+                        <div class="row">
+                            <div class="large-12 columns" style="text-align: center;">
+                                A REVIEW OF ACADEMIC YEAR 2015-2016
+                            </div>
+                        </div>
+        			</div>
+                </section>
 
-        <section class="mc2017-front-cover-photo">
-            <div class="row">
-                <div class="large-12 columns">
-                    <img src="images/COVERWORDART.png" alt="MC 2017 Cover">
-                </div>
-            </div>
-        </section>
+                <section class="mc2017-front-cover-photo">
+                    <div class="row">
+                        <div class="large-12 columns">
+                            <img src="images/COVERWORDART.png" alt="MC 2017 Cover">
+                        </div>
+                    </div>
+                </section>
 
         <?php include 'includes/footer.php'; ?>
     </body>
